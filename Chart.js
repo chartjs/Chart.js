80ac8123cb5929a084bd5dbf68114a9fe6a751f0
Ole Kröger <o.kroeger@wikunia.de>
2013-07-13 19:42:36 +0200

    Correct label position line&bar

--- Chart.js
+++ Chart.js
@@ -879,14 +879,14 @@ window.Chart = function(context){
 			for (var i=0; i<data.labels.length; i++){
 				ctx.save();
 				if (rotateLabels > 0){
-					ctx.translate(yAxisPosX + i*valueHop,xAxisPosY + config.scaleFontSize);
+					ctx.translate(yAxisPosX + i*valueHop + valueHop/2,xAxisPosY + config.scaleFontSize);
 					ctx.rotate(-(rotateLabels * (Math.PI/180)));
 					ctx.fillText(data.labels[i], 0,0);
 					ctx.restore();
 				}
-				
+
 				else{
-					ctx.fillText(data.labels[i], yAxisPosX + i*valueHop,xAxisPosY + config.scaleFontSize+3);					
+					ctx.fillText(data.labels[i], yAxisPosX + i*valueHop + valueHop/2,xAxisPosY + config.scaleFontSize+3);					
 				}
 
 				ctx.beginPath();
@@ -1087,12 +1087,12 @@ window.Chart = function(context){
 			for (var i=0; i<data.labels.length; i++){
 				ctx.save();
 				if (rotateLabels > 0){
-					ctx.translate(yAxisPosX + i*valueHop,xAxisPosY + config.scaleFontSize);
+					ctx.translate(yAxisPosX + i*valueHop + valueHop/2,xAxisPosY + config.scaleFontSize);
 					ctx.rotate(-(rotateLabels * (Math.PI/180)));
 					ctx.fillText(data.labels[i], 0,0);
 					ctx.restore();
 				}
-				
+
 				else{
 					ctx.fillText(data.labels[i], yAxisPosX + i*valueHop + valueHop/2,xAxisPosY + config.scaleFontSize+3);					
 				}
