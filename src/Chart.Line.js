d72c98f54b19e24793708fd5d69509c2c6c0ad23
Joel Jeske <jjeske@hand.com>
2014-09-11 13:06:12 -0500

    Added showLabelInterval option
    Added option to specify a number of XAxis labels to skip. For example,
    if the chart had 100 data points, you could set this option to 10 and
    show every 10th data point label on the chart.

--- src/Chart.Line.js
+++ src/Chart.Line.js
@@ -194,6 +194,7 @@
 				gridLineColor : (this.options.scaleShowGridLines) ? this.options.scaleGridLineColor : "rgba(0,0,0,0)",
 				padding: (this.options.showScale) ? 0 : this.options.pointDotRadius + this.options.pointDotStrokeWidth,
 				showLabels : this.options.scaleShowLabels,
+				showLabelInterval : this.options.showLabelInterval,
 				display : this.options.showScale
 			};
 
