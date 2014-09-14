17dd3676ce693d7d017cc9c7ad555c6c9c2c73b4
Leigh Quince <leighquince@quincewebdesign.com>
2014-09-14 13:32:44 +0200

    Removed defaultConfig from overlay chart
    instead now merging the defaults from bar and line

--- src/Chart.Overlay.js
+++ src/Chart.Overlay.js
@@ -4,76 +4,14 @@
 	var root = this,
 		Chart = root.Chart,
 		helpers = Chart.helpers;
-
-
-	var defaultConfig = {
-		//Function - Whether the current x-axis label should be filtered out, takes in current label and 
-		//index, return true to filter out the label return false to keep the label
-		labelsFilter : function(label,index){return false;},
-
-		//Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-		scaleBeginAtZero : true,
-
-		//Boolean - Whether grid lines are shown across the chart
-		scaleShowGridLines : true,
-
-		//String - Colour of the grid lines
-		scaleGridLineColor : "rgba(0,0,0,.05)",
-
-		//Number - Width of the grid lines
-		scaleGridLineWidth : 1,
-
-		//Boolean - If there is a stroke on each bar
-		barShowStroke : true,
-
-		//Number - Pixel width of the bar stroke
-		barStrokeWidth : 2,
-
-		//Number - Spacing between each of the X value sets
-		barValueSpacing : 5,
-
-		//Number - Spacing between data sets within X values
-		barDatasetSpacing : 1,
-
-		//Boolean - Whether the line is curved between points
-		bezierCurve : true,
-
-		//Number - Tension of the bezier curve between points
-		bezierCurveTension : 0.4,
-
-		//Boolean - Whether to show a dot for each point
-		pointDot : true,
-
-		//Number - Radius of each point dot in pixels
-		pointDotRadius : 4,
-
-		//Number - Pixel width of point dot stroke
-		pointDotStrokeWidth : 1,
-
-		//Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-		pointHitDetectionRadius : 20,
-
-		//Boolean - Whether to show a stroke for datasets
-		datasetStroke : true,
-
-		//Number - Pixel width of dataset stroke
-		datasetStrokeWidth : 2,
-
-		//Boolean - Whether to fill the dataset with a colour
-		datasetFill : true,
-
-		//String - A legend template
-		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
-	};
-
 	//Overlay is a extention of the bar graph to allow the combination
 	//of both line and bar charts on the same graph.
 	//this graph extends the bar graph as the bar graphs scale is used due to 
 	//it needing to detrmine x axis label width.
 	Chart.types.Bar.extend({
 		name: "Overlay",
-		defaults : defaultConfig,
+		//merge defaults from both bar and line
+		defaults : helpers.merge(Chart.types.Bar.prototype.defaults,helpers.merge(Chart.types.Line.prototype.defaults,{})),
 		initialize:  function(data){
 			//Expose options as a scope variable here so we can access it in the ScaleClass
 			var options = this.options;
@@ -135,7 +73,7 @@
 				});
 			}
 
-			//Declare the extension of the default point, to cater for the options passed in to the constructor
+			//Declare the extension of the default Bar, to cater for the options passed in to the constructor
 			this.BarClass = Chart.Rectangle.extend({
 				strokeWidth : this.options.barStrokeWidth,
 				showStroke : this.options.barShowStroke,
@@ -251,7 +189,8 @@
 
 
 		getDataAtEvent : function(e)
-		{
+		{	
+				//find points and bars at event, return as one array
 				return this.getPointsAtEvent(e).concat(this.getBarsAtEvent(e));
 		},
 		getPointsAtEvent : function(e){
