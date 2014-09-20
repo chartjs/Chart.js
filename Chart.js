84647fa149c80b4a8bb2c66a8cdb2c86f8ee9f55
Jacob Lauzier <lauzierj@gmail.com>
2014-09-20 08:12:58 -0400

    made the change in the right spot and then built

--- Chart.js
+++ Chart.js
@@ -92,8 +92,8 @@
 			// Boolean - whether or not the chart should be responsive and resize when the browser does.
 			responsive: false,
 
-			// Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-			maintainAspectRatio: true,
+                        // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+                        maintainAspectRatio: true,
 
 			// Boolean - Determines whether to draw tooltips on the canvas or not - attaches events to touchmove & mousemove
 			showTooltips: true,
@@ -982,7 +982,7 @@
 					each(ChartElements, function(Element) {
 						var tooltipPosition = Element.tooltipPosition();
 						var text = template(this.options.tooltipTemplate, Element);
-						if(text != '') {
+						if(text) {
 							new Chart.Tooltip({
 								x: Math.round(tooltipPosition.x),
 								y: Math.round(tooltipPosition.y),
