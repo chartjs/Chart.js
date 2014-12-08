37b72c7369696dcb4b379f124a85a455b2998e0d
saintedlama <christoph.walcher@gmail.com>
2014-12-08 12:01:44 +0100

    Use graphicsmagick compare functionality to ensure images are somehow equal

--- test/ui/helpers/screenshots-should-match.js
+++ test/ui/helpers/screenshots-should-match.js
@@ -1,12 +1,11 @@
 var path = require('path');
-var fs = require('fs');
 
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs');
 var binPath = phantomjs.path;
 
-var buffertools = require('buffertools');
 var expect = require('chai').expect;
+var gm = require('gm');
 
 module.exports = function screenshotsShouldMatch(chartFile, expectedFile, done) {
   var outputFile = path.join(__dirname, '../../_tmp', expectedFile);
@@ -25,11 +24,21 @@ module.exports = function screenshotsShouldMatch(chartFile, expectedFile, done) 
 
     expect(err).to.not.exist;
 
+    gm.compare(expectedPath, outputFile, function(err, isEqual, equality, raw) {
+      expect(err).to.not.exist;
+      expect(isEqual).to.be.true;
+
+      console.log('equality', equality, raw);
+
+      done();
+    });
+    /*
     var actual = fs.readFileSync(outputFile);
     var expected = fs.readFileSync(expectedPath);
 
     expect(buffertools.compare(expected, actual)).to.equal(0);
 
     done();
+    */
   });
 };
