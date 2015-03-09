de564326b098583bf4edd95d698d6c97a8e612b3
Nick Downie <hello@nickdownie.com>
2015-03-09 17:14:09 +0000

    Target src files from #142

--- src/Chart.Core.js
+++ src/Chart.Core.js
@@ -25,6 +25,25 @@
 		this.ctx = context;
 
 		//Variables global to the chart
+		var computeDimension = function(element,dimension)
+		{
+			if (element['offset'+dimension])
+			{
+				return element['offset'+dimension];
+			}
+			else
+			{
+				return document.defaultView.getComputedStyle(element).getPropertyValue(dimension);
+			}
+		}
+
+		var width = this.width = computeDimension(context.canvas,'Width');
+		var height = this.height = computeDimension(context.canvas,'Height');
+
+		// Firefox requires this to work correctly
+		context.canvas.width  = width;
+		context.canvas.height = height;
+
 		var width = this.width = context.canvas.width;
 		var height = this.height = context.canvas.height;
 		this.aspectRatio = this.width / this.height;
