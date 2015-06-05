e63d5a336766c8e6acbdd4a048a3f4aaccbc5c01
Evert Timberg <evert.timberg@gmail.com>
2015-06-05 18:03:35 -0400

    Only skip when the data is null, not when it is not a number

--- src/Chart.Core.js
+++ src/Chart.Core.js
@@ -1443,7 +1443,7 @@
 
             // Put into the range of (-PI/2, 3PI/2]
             var startAngle = vm.startAngle < (-0.5 * Math.PI) ? vm.startAngle + (2.0 * Math.PI) : vm.startAngle;
-            var endAngle = vm.endAngle < (-0.5 * Math.PI) ? vm.endAngle + (2.0 * Math.PI) : vm.endAngle
+            var endAngle = vm.endAngle < (-0.5 * Math.PI) ? vm.endAngle + (2.0 * Math.PI) : vm.endAngle;
 
             //Check if within the range of the open/close angle
             var betweenAngles = (pointRelativePosition.angle >= startAngle && pointRelativePosition.angle <= endAngle),
