925e3051846572d553c3eed89e95a5db8f3c9fae
Evert Timberg <evert.timberg@gmail.com>
2015-07-05 14:56:22 -0400

    Doughnut legends (only makes sense for first dataset).

--- src/charts/Chart.Doughnut.js
+++ src/charts/Chart.Doughnut.js
@@ -5,7 +5,12 @@
 	var Chart = root.Chart;
 	var helpers = Chart.helpers;
 
+	var defaultConfig = {
+		legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i = 0; i < data.datasets[0].data.length; i++){%><li><span style=\"background-color:<%=data.datasets[0].backgroundColor[i]%>\"><%if(data.labels && i < data.labels.length){%><%=data.labels[i]%><%}%></span></li><%}%></ul>",
+	};
+
 	Chart.Doughnut = function(context, config) {
+		config.options = helpers.configMerge(defaultConfig, config.options);
 		config.type = 'doughnut';
 
 		return new Chart(context, config);
