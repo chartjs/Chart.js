54858b921d2fbe67f02bfad95fde7abe68b1ecc0
Evert Timberg <evert.timberg@gmail.com>
2015-08-31 20:42:29 -0400

    More linear scale tests

--- test/mockContext.js
+++ test/mockContext.js
@@ -68,6 +68,7 @@
 			clearRect: function() {},
 			closePath: function() {},
 			fill: function() {},
+			fillText: function() {},
 			lineTo: function(x, y) {},
 			measureText: function(text) {
 				// return the number of characters * fixed size
@@ -107,6 +108,10 @@
 	Context.prototype.getCalls = function() {
 		return this._calls;
 	}
+
+	Context.prototype.resetCalls = function() {
+		this._calls = [];
+	};
 
 	window.createMockContext = function() {
 		return new Context();
