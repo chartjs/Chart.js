3ac54b87900a951d7b97ba2b8bcf5e975797e49e
coolbombom <cool_bombom@hotmail.com>
2016-01-11 12:02:32 +0100

    having differnt colors in one dataset
    Hi updated the bar chart so the bars can have different colors in one dataset!
    
    cheers
    c_bb

--- src/Chart.Bar.js
+++ src/Chart.Bar.js
@@ -115,10 +115,10 @@
 						value : dataPoint,
 						label : data.labels[index],
 						datasetLabel: dataset.label,
-						strokeColor : dataset.strokeColor,
-						fillColor : dataset.fillColor,
-						highlightFill : dataset.highlightFill || dataset.fillColor,
-						highlightStroke : dataset.highlightStroke || dataset.strokeColor
+						strokeColor : dataset.strokeColor[index] || dataset.strokeColor,
+						fillColor : dataset.fillColor[index] || dataset.fillColor,
+						highlightFill : dataset.highlightFill[index] || dataset.highlightFill || dataset.fillColor[index] || dataset.fillColor,
+						highlightStroke : dataset.highlightStroke[index] || dataset.highlightStroke || dataset.strokeColor[index] || dataset.strokeColor
 					}));
 				},this);
 
