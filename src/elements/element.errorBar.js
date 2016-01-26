11cb587baa4a083e804e2533c7c970758d2a3b6d
CAYdenberg <casey@graphos.ca>
2016-01-26 16:57:36 -0700

    Add boilerplate for ErrorBar chart element

---
+++ src/elements/element.errorBar.js
@@ -0,0 +1,16 @@
+(function() {
+
+	"use strict";
+
+	var root = this,
+		Chart = root.Chart,
+		helpers = Chart.helpers;
+
+    Chart.elements.ErrorBar = Chart.Element.extend({
+      draw: function() {
+
+      }
+    })
+
+
+}).call(this);
