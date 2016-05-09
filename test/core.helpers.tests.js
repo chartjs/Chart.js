170fdab6a608b69594b60fa1fde3e3afc8b0ff2b
Tom Loudon <tomloudon@gmail.com>
2016-05-09 07:24:32 +0100

    Removed 'background' from hover color helper name.
    Patterns could be used for style attributes other than background e.g. stroke. Updates  chartjs/Chart.js#1323

--- test/core.helpers.tests.js
+++ test/core.helpers.tests.js
@@ -684,8 +684,8 @@ describe('Core helper tests', function() {
 		});
 	});
 
-	describe('Background canvas hover helper', function() {
-		it('should return a CanvasPattern backgroundColor when called with a CanvasPattern', function(done) {
+	describe('Background hover color helper', function() {
+		it('should return a CanvasPattern when called with a CanvasPattern', function(done) {
 			var dots = new Image();
 			dots.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAMAAAAolt3jAAAAD1BMVEUAAAD///////////////+PQt5oAAAABXRSTlMAHlFhZsfk/BEAAAAqSURBVHgBY2BgZGJmYmSAAUYWEIDzmcBcJhiXGcxlRpPFrhdmMiqgvX0AcGIBEUAo6UAAAAAASUVORK5CYII=';
 			dots.onload = function() {
@@ -695,7 +695,7 @@ describe('Core helper tests', function() {
 				var pattern = patternContext.createPattern(dots, 'repeat');
 				patternContext.fillStyle = pattern;
 
-				var backgroundColor = helpers.getHoverBackgroundColor(chartContext.createPattern(patternCanvas, 'repeat'));
+				var backgroundColor = helpers.getHoverColor(chartContext.createPattern(patternCanvas, 'repeat'));
 
 				expect(backgroundColor instanceof CanvasPattern).toBe(true);
 
@@ -703,10 +703,10 @@ describe('Core helper tests', function() {
 			}
 		});
 
-		it('should return a modified version of backgroundColor when called with a color', function() {
+		it('should return a modified version of color when called with a color', function() {
 			var originalColorRGB = 'rgb(70, 191, 189)';
 
-			expect(helpers.getHoverBackgroundColor('#46BFBD')).not.toEqual(originalColorRGB);
+			expect(helpers.getHoverColor('#46BFBD')).not.toEqual(originalColorRGB);
 		});
 	});
 });
