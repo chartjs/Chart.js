46fc96bf4d97989096aa1024be3d0ee2c9a6a789
Evert Timberg <evert.timberg+github@gmail.com>
2016-05-25 21:30:22 -0400

    Remove unused code from core controller.

--- src/core/core.controller.js
+++ src/core/core.controller.js
@@ -53,7 +53,6 @@ module.exports = function(Chart) {
 			this.ensureScalesHaveIDs();
 			this.buildOrUpdateControllers();
 			this.buildScales();
-			this.buildSurroundingItems();
 			this.updateLayout();
 			this.resetElements();
 			this.initToolTip();
@@ -164,18 +163,6 @@ module.exports = function(Chart) {
 			});
 
 			Chart.scaleService.addScalesToLayout(this);
-		},
-
-		buildSurroundingItems: function() {
-			/*if (this.options.title) {
-				this.titleBlock = new Chart.Title({
-					ctx: this.chart.ctx,
-					options: this.options.title,
-					chart: this
-				});
-
-				Chart.layoutService.addBox(this, this.titleBlock);
-			}*/
 		},
 
 		updateLayout: function() {
