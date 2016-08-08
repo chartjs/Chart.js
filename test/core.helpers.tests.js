ef66bf5e6d1293e9ccc104c87f01f9578e5eea15
MatthieuRivaud <matthieu.rivaud+github@gmail.com>
2016-08-08 16:33:32 +0200

    Fixed splineCurveMonotone unit test

--- test/core.helpers.tests.js
+++ test/core.helpers.tests.js
@@ -442,21 +442,137 @@ describe('Core helper tests', function() {
 			{ x:  36, y: 170, skip: false }
 		];
 		helpers.splineCurveMonotone(dataPoints);
-		expect(dataPoints).toEqual([
-			{ x:   0, y:   0, skip: false, controlPointPreviousX: undefined, controlPointPreviousY: undefined, controlPointNextX: 1        , controlPointNextY: 2         },
-			{ x:   3, y:   6, skip: false, controlPointPreviousX: 2        , controlPointPreviousY: 6        , controlPointNextX: 5        , controlPointNextY: 6         },
-			{ x:   9, y:   6, skip: false, controlPointPreviousX: 7        , controlPointPreviousY: 6        , controlPointNextX: 10       , controlPointNextY: 6         },
-			{ x:  12, y:  60, skip: false, controlPointPreviousX: 11       , controlPointPreviousY: 60       , controlPointNextX: 13       , controlPointNextY: 60        },
-			{ x:  15, y:  60, skip: false, controlPointPreviousX: 14       , controlPointPreviousY: 60       , controlPointNextX: 16       , controlPointNextY: 60        },
-			{ x:  18, y: 120, skip: false, controlPointPreviousX: 17       , controlPointPreviousY: 100      , controlPointNextX: undefined, controlPointNextY: undefined },
-			{ x: NaN, y: NaN, skip: true , controlPointPreviousX: undefined, controlPointPreviousY: undefined, controlPointNextX: undefined, controlPointNextY: undefined },
-			{ x:  21, y: 180, skip: false, controlPointPreviousX: undefined, controlPointPreviousY: undefined, controlPointNextX: 22       , controlPointNextY: 160       },
-			{ x:  24, y: 120, skip: false, controlPointPreviousX: 23       , controlPointPreviousY: 120      , controlPointNextX: 25       , controlPointNextY: 120       },
-			{ x:  27, y: 125, skip: false, controlPointPreviousX: 26       , controlPointPreviousY: 125      , controlPointNextX: 28       , controlPointNextY: 125       },
-			{ x:  30, y: 105, skip: false, controlPointPreviousX: 29       , controlPointPreviousY: 105      , controlPointNextX: 31       , controlPointNextY: 105       },
-			{ x:  33, y: 110, skip: false, controlPointPreviousX: 32       , controlPointPreviousY: 105      , controlPointNextX: 34       , controlPointNextY: 115       },
-			{ x:  36, y: 170, skip: false, controlPointPreviousX: 35       , controlPointPreviousY: 150      , controlPointNextX: undefined, controlPointNextY: undefined }
-		]);
+		expect(dataPoints).toEqual([{
+			_model: {
+				x: 0,
+				y: 0,
+				skip: false,
+				controlPointNextX: 1,
+				controlPointNextY: 2
+			}
+		},
+		{
+			_model: {
+				x: 3,
+				y: 6,
+				skip: false,
+				controlPointPreviousX: 2,
+				controlPointPreviousY: 6,
+				controlPointNextX: 5,
+				controlPointNextY: 6
+			}
+		},
+		{
+			_model: {
+				x: 9,
+				y: 6,
+				skip: false,
+				controlPointPreviousX: 7,
+				controlPointPreviousY: 6,
+				controlPointNextX: 10,
+				controlPointNextY: 6
+			}
+		},
+		{
+			_model: {
+				x: 12,
+				y: 60,
+				skip: false,
+				controlPointPreviousX: 11,
+				controlPointPreviousY: 60,
+				controlPointNextX: 13,
+				controlPointNextY: 60
+			}
+		},
+		{
+			_model: {
+				x: 15,
+				y: 60,
+				skip: false,
+				controlPointPreviousX: 14,
+				controlPointPreviousY: 60,
+				controlPointNextX: 16,
+				controlPointNextY: 60
+			}
+		},
+		{
+			_model: {
+				x: 18,
+				y: 120,
+				skip: false,
+				controlPointPreviousX: 17,
+				controlPointPreviousY: 100
+			}
+		},
+		{
+			_model: {
+				x: null,
+				y: null,
+				skip: true
+			}
+		},
+		{
+			_model: {
+				x: 21,
+				y: 180,
+				skip: false,
+				controlPointNextX: 22,
+				controlPointNextY: 160
+			}
+		},
+		{
+			_model: {
+				x: 24,
+				y: 120,
+				skip: false,
+				controlPointPreviousX: 23,
+				controlPointPreviousY: 120,
+				controlPointNextX: 25,
+				controlPointNextY: 120
+			}
+		},
+		{
+			_model: {
+				x: 27,
+				y: 125,
+				skip: false,
+				controlPointPreviousX: 26,
+				controlPointPreviousY: 125,
+				controlPointNextX: 28,
+				controlPointNextY: 125
+			}
+		},
+		{
+			_model: {
+				x: 30,
+				y: 105,
+				skip: false,
+				controlPointPreviousX: 29,
+				controlPointPreviousY: 105,
+				controlPointNextX: 31,
+				controlPointNextY: 105
+			}
+		},
+		{
+			_model: {
+				x: 33,
+				y: 110,
+				skip: false,
+				controlPointPreviousX: 32,
+				controlPointPreviousY: 105,
+				controlPointNextX: 34,
+				controlPointNextY: 115
+			}
+		},
+		{
+			_model: {
+				x: 36,
+				y: 170,
+				skip: false,
+				controlPointPreviousX: 35,
+				controlPointPreviousY: 150
+			}
+		}]);
 	});
 
 	it('should get the next or previous item in an array', function() {
