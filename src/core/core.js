55b3c9af5fb5431cd9ef338960f3a7308f2c9aae
Tom Dodson <t3.dodson@gmail.com>
2016-09-13 22:18:22 -0700

    This is a more declarative way to check if the context is a jQuery element.
    Also no need to check .getContext twice.

--- src/core/core.js
+++ src/core/core.js
@@ -13,7 +13,7 @@ module.exports = function() {
 		};
 
 		// Support a jQuery'd canvas element
-		if (context.length && context[0].getContext) {
+		if (typeof(jQuery) === 'function' && context instanceof jQuery) {
 			context = context[0];
 		}
 
