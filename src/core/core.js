dab50061a3644dd5af106bde80c71cdc3e647982
dibsyjr1 <dibsy-jr@hotmail.co.uk>
2016-09-22 10:47:19 +0100

    Sorted out a couple of errors returned from the build check
    Sorted out the closing curly braces issue and hopefully the trailing spaces issue

--- src/core/core.js
+++ src/core/core.js
@@ -91,12 +91,11 @@ module.exports = function() {
 				text.push('<ul class="' + chart.id + '-legend">');
 				for (var i = 0; i < chart.data.datasets.length; i++) {
 					if (chart.options.legend.fontFamily) {
-						text.push('<li style="font-family:' + chart.options.legend.fontFamily + ';">');
-					}
-					else {
-						text.push('<li style="font-family:' + Chart.defaults.global.legend.labels.fontFamily + ';">');
+						text.push('<li style="font-family:'+chart.options.legend.fontFamily+';">');
+					} else {
+						text.push('<li style="font-family:'+Chart.defaults.global.legend.labels.fontFamily+';">');
 					}        
-					text.push('<span style="background-color:' + chart.data.datasets[i].backgroundColor + '; border: 3px solid ' + chart.data.datasets[i].borderColor + '; width: 42px; height: 14px;"></span>');
+					text.push('<span style="background-color:'+chart.data.datasets[i].backgroundColor+'; border: 3px solid '+chart.data.datasets[i].borderColor+'; width: 42px; height: 14px;"></span>');
 					if (chart.data.datasets[i].label) {
 						text.push(chart.data.datasets[i].label);
 					}
