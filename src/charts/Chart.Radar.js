d610987cfbc2ed37d5b2d508b1378f9cb401edd2
Simon Brunel <simonbrunel@users.noreply.github.com>
2016-09-23 17:44:06 +0200

    Fix radar default aspect ratio and samples
    Now that the aspect ratio is correctly handled, fix samples for charts with aspect ratio of 1 which was vertically too large. Also fix the default aspect ratio for radar charts which wasn't applied when creating a chart directly using new Chart(ctx, { type: 'radar' }).

--- src/charts/Chart.Radar.js
+++ src/charts/Chart.Radar.js
@@ -3,7 +3,6 @@
 module.exports = function(Chart) {
 
 	Chart.Radar = function(context, config) {
-		config.options = Chart.helpers.configMerge({aspectRatio: 1}, config.options);
 		config.type = 'radar';
 
 		return new Chart(context, config);
