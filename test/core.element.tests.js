3df130afd6099f1de81776740b53ccfe8ab5eb30
Jerry Chang <therealjerrycan@gmail.com>
2016-11-14 20:30:18 -0800

    fixed tooltip labelling on Bar Chart when min is defined
    added helper method to adjust the index
    
    pass in chartConfig rather than access within method, make it easier to
    test
    
    added semi-colon at the end of helper method
    
    added test for adjustIndex helper method
    
    fixed lint issues
    
    added integration test for the interaction of trigger an event over the
    bar
    
    .
    
    .
    
    moved adjustIndex into element helper
    
    removed method from helper and adjusted method in core.interaction
    
    added test for the element adjustIndex helper
    
    added a skipIndexAdjustment method to handle when to skip the adjustment
    along with test cases
    
    fixed lint issues
    
    removed the test for the helper method

--- test/core.element.tests.js
+++ test/core.element.tests.js
@@ -42,4 +42,88 @@ describe('Core element tests', function() {
 			colorProp: 'rgb(64, 64, 0)',
 		});
 	});
+
+	it ('should adjust the index of the element passed in', function() {
+		var chartConfig = {
+			options: {
+				scales: {
+					xAxes: [{
+						ticks: {
+							min: 'Point 2'
+						}
+					}]
+				}
+			},
+			data: {
+				labels: ['Point 1', 'Point 2', 'Point 3']
+			}
+		};
+
+		var element = new Chart.Element({
+			_index: 1
+		});
+
+		element.adjustIndex(chartConfig);
+
+		expect(element._adjustedIndex).toEqual(true);
+		expect(element._index).toEqual(0);
+	});
+
+	describe ('skipIndexAdjustment method', function() {
+		var element;
+
+		beforeEach(function() {
+			element = new Chart.Element({});
+		});
+
+		it ('should return true when min is undefined', function() {
+			var chartConfig = {
+				options: {
+					scales: {
+						xAxes: [{
+							ticks: {
+								min: undefined
+							}
+						}]
+					}
+				}
+			};
+			expect(element.skipIndexAdjustment(chartConfig)).toEqual(true);
+		});
+
+		it ('should return true when index is already adjusted (_adjustedIndex = true)', function() {
+			var chartConfig = {
+				options: {
+					scales: {
+						xAxes: [{
+							ticks: {
+								min: 'Point 1'
+							}
+						}]
+					}
+				}
+			};
+			element._adjustedIndex = true;
+			expect(element.skipIndexAdjustment(chartConfig)).toEqual(true);
+		});
+
+		it ('should return true when more than one xAxes is defined', function() {
+			var chartConfig = {
+				options: {
+					scales: {
+						xAxes: [{
+							ticks: {
+								min: 'Point 1'
+							}
+						}, {
+							ticks: {
+								min: 'Point 2'
+							}
+						}]
+					}
+				}
+			};
+			expect(element.skipIndexAdjustment(chartConfig)).toEqual(true);
+		});
+	});
 });
