9db4950b30a00d694c99fbd5e2bcb6b9e8842cc1
Catalin Pintea <catalin@rebs-group.com>
2017-02-27 17:32:15 +0200

    Cleaner width check & add some spaces for readability

--- src/controllers/controller.bar.js
+++ src/controllers/controller.bar.js
@@ -168,14 +168,13 @@ module.exports = function(Chart) {
 			var xScale = me.getScaleForId(meta.xAxisID);
 			var maxBarThickness = xScale.options.maxBarThickness;
 			var barWidth;
+
 			if (xScale.options.barThickness) {
 				return xScale.options.barThickness;
 			}
+
 			barWidth = xScale.options.stacked ? ruler.categoryWidth * xScale.options.barPercentage : ruler.barWidth;
-			if (maxBarThickness && (barWidth > maxBarThickness)) {
-				return maxBarThickness;
-			}
-			return barWidth;
+			return Math.min(barWidth, maxBarThickness || Infinity);
 		},
 
 		// Get stack index from the given dataset index accounting for stacks and the fact that not all bars are visible
