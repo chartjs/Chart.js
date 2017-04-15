254bd4bf86ab8dc9801c61844cd3e668e3e2090f
GabrielMancik <gabriel.mancik@gmail.com>
2017-04-15 18:37:49 +0200

    Fixed calculation of scale min and max when dataset contains no values (#4064)
    * Fixed different calculation of scale min and max when dataset contains no values
    * Removed trailing spaces
    * Added test for correct min/max calculation
    * Removed trailing spaces

--- test/specs/scale.linear.tests.js
+++ test/specs/scale.linear.tests.js
@@ -117,6 +117,35 @@ describe('Linear Scale', function() {
 		expect(chart.scales.yScale0.max).toBe(150);
 	});
 
+	it('Should correctly determine the max & min when no values provided and suggested minimum and maximum are set', function() {
+		var chart = window.acquireChart({
+			type: 'bar',
+			data: {
+				datasets: [{
+					yAxisID: 'yScale0',
+					data: []
+				}],
+				labels: ['a', 'b', 'c', 'd', 'e', 'f']
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						id: 'yScale0',
+						type: 'linear',
+						ticks: {
+							suggestedMin: -10,
+							suggestedMax: 15
+						}
+					}]
+				}
+			}
+		});
+
+		expect(chart.scales.yScale0).not.toEqual(undefined); // must construct
+		expect(chart.scales.yScale0.min).toBe(-10);
+		expect(chart.scales.yScale0.max).toBe(15);
+	});
+
 	it('Should correctly determine the max & min data values ignoring hidden datasets', function() {
 		var chart = window.acquireChart({
 			type: 'bar',
