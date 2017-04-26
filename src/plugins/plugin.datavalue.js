d683b4df3a15980c538293b8f9fbea900dbc5f34
apoorvasrinivasan <ascoolas.apoorva@gmail.com>
2017-04-26 17:50:48 +0530

    dataValue plugin wth extra options. using getCenter point

--- src/plugins/plugin.datavalue.js
+++ src/plugins/plugin.datavalue.js
@@ -4,30 +4,42 @@ module.exports = function(Chart) {
 
 	var helpers = Chart.helpers;
 	// var noop = helpers.noop;
-	Chart.defaults.global.dataLabels = {
+	Chart.defaults.global.valueLabel = {
 		// position: 'top',
 		fontWeight: 'normal',
-		fontColor: '#000'
+		fontColor: '#000',
+		fontSize: 10,
+		padding: {top: 0, left: 0}
 		// truncate: true
 	};
-	function DrawLabel(chart, element, value, index) {
-		var opts = chart.options.dataLabels;
+	function DrawValue(chart, element, value, index, chartype) {
+		var opts = chart.options.valueLabel;
 		var ctx = chart.ctx;
-		var position = element.tooltipPosition();
-		var top = position.y;
-		var left = position.x;
+		var position = element.getCenterPoint();
+		// print the value on the top of the bars.
+		if (chartype === 'bar') {
+			position = element.tooltipPosition();
+			position.y += opts.fontSize;
+		}
+		var top = position.y + opts.padding.top;
+		var left = position.x+ opts.padding.left;
+		// set the font of the context
 		ctx.textAlign='center';
 		var fillStyle = opts.fontColor;
 		if (typeof fillStyle === 'object') {
 			fillStyle = fillStyle[index] || '#000';
 		}
 		ctx.fillStyle = fillStyle;
-		ctx.font=helpers.fontString(10, opts.fontWeight);
+		ctx.font=helpers.fontString(opts.fontSize, opts.fontWeight);
+		// get the value to be displayed.
+		if (opts.value) {
+			value = opts.value(chart, value);
+		}
 		ctx.fillText(value, left, top);
 
 	}
 	return {
-		id: 'dataLabels',
+		id: 'valueLabel',
 		afterDatasetsDraw: function(chart) {
 			chart.data.datasets.forEach(function(dataset, i) {
 				var elements = chart.getDatasetMeta(i);
@@ -36,8 +48,8 @@ module.exports = function(Chart) {
 					elements.data.forEach(function(element, index) {
 						var d = dataset.data[index];
 						if (d !== null && d !== undefined && !isNaN(d)) {
-							if (dataset.drawLabel) {
-								DrawLabel(chart, element, d.toString(), index);
+							if (dataset.drawValue) {
+								DrawValue(chart, element, d.toString(), index, elements.type);
 							}
 						}
 					});
