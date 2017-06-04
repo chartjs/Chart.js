fd49ac9200ba91ca427766a479cc399dcc5678dd
Evert Timberg <evert.timberg+github@gmail.com>
2017-06-04 13:32:44 -0400

    Provide a blank default global layout option (#4319)

--- src/core/core.js
+++ src/core/core.js
@@ -32,6 +32,16 @@ module.exports = function() {
 			// Element defaults defined in element extensions
 			elements: {},
 
+			// Layout options such as padding
+			layout: {
+				padding: {
+					top: 0,
+					right: 0,
+					bottom: 0,
+					left: 0
+				}
+			},
+
 			// Legend callback string
 			legendCallback: function(chart) {
 				var text = [];
