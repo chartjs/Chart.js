8cbda4965784414dc8cef53fda75ffc3c099a0bf
dkellis42 <ellisd@dnb.com>
2017-08-01 10:37:10 -0500

    Modify splitNewlines method to not break regression

--- src/core/core.tooltip.js
+++ src/core/core.tooltip.js
@@ -123,14 +123,14 @@ module.exports = function(Chart) {
 	/**
 	 * Returns array of strings split by newline
 	 * @param {String} value - The value to split by newline.
-	 * @returns {Array} value - Returned from String split() method
+	 * @returns {Array} value if newline present - Returned from String split() method
 	 * @function
 	 */
 	function splitNewlines(str) {
-		if (typeof str === 'string' || str instanceof String) {
+		if ((typeof str === 'string' || str instanceof String) && str.indexOf('\n') > -1) {
 			return str.split('\n');
 		}
-		return str || [];
+		return str;
 	}
 
 	// Private helper to create a tooltip item model
