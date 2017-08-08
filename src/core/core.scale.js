de9f7fab9ed6a83dd6110734c0dd311dc5d0e269
andig <cpuidle@gmx.de>
2017-08-08 22:52:34 +0200

    Add axis to callback

--- src/core/core.scale.js
+++ src/core/core.scale.js
@@ -298,7 +298,7 @@ module.exports = function(Chart) {
 			var me = this;
 			// Convert ticks to strings
 			var tickOpts = me.options.ticks;
-			me.ticks = me.ticks.map(tickOpts.userCallback || tickOpts.callback);
+			me.ticks = me.ticks.map(tickOpts.userCallback || tickOpts.callback, this);
 		},
 		afterTickToLabelConversion: function() {
 			helpers.callback(this.options.afterTickToLabelConversion, [this]);
