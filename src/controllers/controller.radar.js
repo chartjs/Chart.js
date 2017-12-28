88ec1f2b2be5c5a1e8ece7c2aec31b48d5faa487
flaurida <lauracressman@mac.com>
2017-12-28 18:29:49 -0500

    Fix issue with span gaps option

--- src/controllers/controller.radar.js
+++ src/controllers/controller.radar.js
@@ -52,8 +52,7 @@ module.exports = function(Chart) {
 				_model: {
 					// Appearance
 					// This option gives lines the ability to span gaps
-					spanGaps: dataset.spanGaps ? dataset.spanGaps : options.spanGaps,
-					tension: custom.tension ? custom.tension : helpers.valueOrDefault(dataset.lineTension, lineElementOptions.tension),
+					spanGaps: (dataset.spanGaps !== undefined ? dataset.spanGaps : options.spanGaps),
 					backgroundColor: custom.backgroundColor ? custom.backgroundColor : (dataset.backgroundColor || lineElementOptions.backgroundColor),
 					borderWidth: custom.borderWidth ? custom.borderWidth : (dataset.borderWidth || lineElementOptions.borderWidth),
 					borderColor: custom.borderColor ? custom.borderColor : (dataset.borderColor || lineElementOptions.borderColor),
