df987a29ac88e09b7593c7d656e9747b6007908a
loicbourgois <loic@bourgois.pro>
2018-01-13 20:25:56 +0000

    Add tests for showLines for scatter chart

---
+++ test/specs/controller.scatter.test.js
@@ -0,0 +1,77 @@
+describe('Chart.controllers.line', function() {
+
+	fit('should not draw a line if the options showLines is not set', function() {
+		var chart = window.acquireChart({
+			type: 'scatter',
+			data: {
+				datasets: [{
+					data: [
+						{x: 10, y: 15},
+						{x: 0, y: -8},
+						{x: -50, y: -6},
+						{x: 12, y: -4},
+						{x: -9, y: 8}
+					],
+					label: 'dataset1'
+				}],
+			},
+			options: {}
+		});
+
+		var meta = chart.getDatasetMeta(0);
+		spyOn(meta.dataset, 'draw');
+		spyOn(meta.data[0], 'draw');
+		spyOn(meta.data[1], 'draw');
+		spyOn(meta.data[2], 'draw');
+		spyOn(meta.data[3], 'draw');
+		spyOn(meta.data[4], 'draw');
+
+		chart.update();
+
+		expect(meta.dataset.draw.calls.count()).toBe(0);
+		expect(meta.data[0].draw.calls.count()).toBe(1);
+		expect(meta.data[1].draw.calls.count()).toBe(1);
+		expect(meta.data[2].draw.calls.count()).toBe(1);
+		expect(meta.data[3].draw.calls.count()).toBe(1);
+		expect(meta.data[4].draw.calls.count()).toBe(1);
+	});
+
+	fit('should draw a line if the options showLines is set to true', function() {
+		var chart = window.acquireChart({
+			type: 'scatter',
+			data: {
+				datasets: [{
+					data: [
+						{x: 10, y: 15},
+						{x: 0, y: -8},
+						{x: -50, y: -6},
+						{x: 12, y: -4},
+						{x: -9, y: 8}
+					],
+					label: 'dataset1'
+				}],
+			},
+			options: {
+				showLines: true
+			}
+		});
+
+		var meta = chart.getDatasetMeta(0);
+		spyOn(meta.dataset, 'draw');
+		spyOn(meta.data[0], 'draw');
+		spyOn(meta.data[1], 'draw');
+		spyOn(meta.data[2], 'draw');
+		spyOn(meta.data[3], 'draw');
+		spyOn(meta.data[4], 'draw');
+
+		chart.update();
+
+		expect(meta.dataset.draw.calls.count()).toBe(1);
+		expect(meta.data[0].draw.calls.count()).toBe(1);
+		expect(meta.data[1].draw.calls.count()).toBe(1);
+		expect(meta.data[2].draw.calls.count()).toBe(1);
+		expect(meta.data[3].draw.calls.count()).toBe(1);
+		expect(meta.data[4].draw.calls.count()).toBe(1);
+	});
+
+});
