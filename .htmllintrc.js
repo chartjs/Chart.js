13cd5b0c280ee1728ab4c61163de956dc9cb72eb
loicbourgois <loic@bourgois.pro>
2018-01-27 22:25:00 +0000

    Add htmllint

---
+++ .htmllintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'rules': {
+		//'indent-style': 'tabs',
+		//'indent-width': 1,
+		'indent-style': false,
+		'indent-width': 2,
+		'attr-quote-style': 'double',
+		'spec-char-escape': false,
+		'attr-bans': ['align', 'background', 'bgcolor', 'border', 'frameborder', 'longdesc', 'marginwidth', 'marginheight', 'scrolling', 'syle'/*, 'width'*/],
+		'tag-bans': [/*'style', */'b', 'i'],
+		'id-class-style': false,
+	}
+}
