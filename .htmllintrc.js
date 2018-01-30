066b3aefb2a9ee66982e5473b3fc63b21db17129
loicbourgois <loic@bourgois.pro>
2018-01-30 11:10:27 +0000

    Correct typo

--- .htmllintrc.js
+++ .htmllintrc.js
@@ -3,7 +3,7 @@ module.exports = {
 		'indent-style': 'tabs',
 		'attr-quote-style': 'double',
 		'spec-char-escape': false,
-		'attr-bans': ['align', 'background', 'bgcolor', 'border', 'frameborder', 'longdesc', 'marginwidth', 'marginheight', 'scrolling', 'syle'/*, 'width'*/],
+		'attr-bans': ['align', 'background', 'bgcolor', 'border', 'frameborder', 'longdesc', 'marginwidth', 'marginheight', 'scrolling'/*, 'style', 'width'*/],
 		'tag-bans': [/*'style', */'b', 'i'],
 		'id-class-style': false,
 	}
