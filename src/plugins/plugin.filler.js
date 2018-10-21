24ac8702940e4b1af86010ac2d1c5e12464e327b
etimberg <evert.timberg@gmail.com>
2018-10-21 12:01:25 -0400

    Use new helper function

--- src/plugins/plugin.filler.js
+++ src/plugins/plugin.filler.js
@@ -128,7 +128,7 @@ function computeBoundary(source) {
 			return target;
 		}
 
-		if ((typeof target === 'number' || target instanceof Number) && isFinite(target)) {
+		if (helpers.isFinite(target)) {
 			horizontal = scale.isHorizontal();
 			return {
 				x: horizontal ? target : null,
