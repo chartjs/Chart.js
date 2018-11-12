34230324e2ab89f378ccca9934a20ad524f0f16d
Weit <evert.van.der.weit@philips.com>
2018-11-12 08:32:20 +0100

    Implement reviews

--- samples/samples.js
+++ samples/samples.js
@@ -187,9 +187,6 @@
 		}, {
 			title: 'Data labelling (plugin)',
 			path: 'advanced/data-labelling.html'
-		}, {
-			title: 'Realtime data update',
-			path: 'advanced/live-data.html'
 		}]
 	}];
 
