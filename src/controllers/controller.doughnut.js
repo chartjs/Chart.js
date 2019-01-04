e442a733b0d18a588094142ccec51cd1f009c318
Vincent-Ip <vincent.ip.1975@gmail.com>
2019-01-03 22:07:57 -0500

    simplified chartWeight assignment

--- src/controllers/controller.doughnut.js
+++ src/controllers/controller.doughnut.js
@@ -151,7 +151,7 @@ module.exports = DatasetController.extend({
 		var arcs = meta.data;
 		var cutoutPercentage = opts.cutoutPercentage;
 		var circumference = opts.circumference;
-		var chartWeight = chart.data.datasets[me.index].weight ? chart.data.datasets[me.index].weight : 1;
+		var chartWeight = chart.data.datasets[me.index].weight || 1;
 		var i, ilen;
 
 		// If the chart's circumference isn't a full circle, calculate minSize as a ratio of the width/height of the arc
