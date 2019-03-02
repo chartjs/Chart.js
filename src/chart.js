946098fae257726ff63a79d176e155f1f1e51a4b
Simon Brunel <simonbrunel@users.noreply.github.com>
2019-03-02 19:32:01 +0100

    Remove sources from npm package and create alias

---
+++ src/chart.js
@@ -0,0 +1,6 @@
+/**
+ * DEPRECATED since version 2.8, this file is provided for
+ * backward compatibility and will be removed at version 3.
+ * https://github.com/chartjs/Chart.js/pull/6105
+ */
+module.exports = require('../dist/Chart');
