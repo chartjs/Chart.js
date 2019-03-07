9736257e3edcaed455ae9369c61ce118e094e165
Janelle de Ment <janelledement@gmail.com>
2019-03-07 11:04:34 -0400

    Adds backgroundColor tests.

---
+++ test/fixtures/controller.line/backgroundColor/line-indexable.js
@@ -0,0 +1,41 @@
+module.exports = {
+	config: {
+		type: 'line',
+		data: {
+			labels: [0, 1, 2, 3, 4, 5],
+			datasets: [
+				{
+					data: [0, 5, 10, null, -10, -5],
+				},
+				{
+					data: [4, -5, -10, null, 10, 5],
+				}
+			]
+		},
+		options: {
+			legend: false,
+			title: false,
+			elements: {
+				line: {
+					backgroundColor: [
+						'#ff88ff',
+						'#ff8800',
+					]
+				},
+				point: {
+					radius: 10
+				}
+			},
+			scales: {
+				xAxes: [{display: false}],
+				yAxes: [{display: false}]
+			}
+		}
+	},
+	options: {
+		canvas: {
+			height: 256,
+			width: 512
+		}
+	}
+};
