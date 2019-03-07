47604a8d17a95ad2edf3c3f3ce53342bf71cb26f
Janelle de Ment <janelledement@gmail.com>
2019-03-07 14:36:42 -0400

    Adds tests for borderWidth.

---
+++ test/fixtures/controller.line/borderWidth/line-indexable.js
@@ -0,0 +1,43 @@
+module.exports = {
+	config: {
+		type: 'line',
+		data: {
+			labels: [0, 1, 2, 3, 4, 5],
+			datasets: [
+				{
+					// option in dataset
+					data: [0, 5, 10, null, -10, -5],
+					borderWidth: [2]
+				},
+				{
+					// option in element (fallback)
+					data: [4, -5, -10, null, 10, 5],
+				}
+			]
+		},
+		options: {
+			legend: false,
+			title: false,
+			elements: {
+				line: {
+					borderColor: '#0000ff',
+					borderWidth: [2, 10,]
+
+				},
+				point: {
+					radius: 10
+				}
+			},
+			scales: {
+				xAxes: [{display: false}],
+				yAxes: [{display: false}]
+			}
+		}
+	},
+	options: {
+		canvas: {
+			height: 256,
+			width: 512
+		}
+	}
+};
---
+++ test/fixtures/controller.line/borderWidth/line-scriptable.js
@@ -0,0 +1,61 @@
+module.exports = {
+	config: {
+		type: 'line',
+		data: {
+			labels: [0, 1, 2, 3, 4, 5],
+			datasets: [
+				{
+					// option in dataset
+					data: [4, 5, 10, null, -10, -5],
+					borderColor: '#0000ff',
+					borderWidth: function(ctx) {
+						var value = ctx.dataset.data.reduce(function(a, b) {
+							return a + b;
+						});
+						return value > 0 ? 20 : 10;
+					}
+				},
+				{
+					// option in element (fallback)
+					data: [-4, -5, -10, null, 10, 5],
+				}
+			]
+		},
+		options: {
+			legend: false,
+			title: false,
+			elements: {
+				line: {
+					fill: false,
+					borderColor: '#ff0000',
+					borderWidth: function(ctx) {
+						var value = ctx.dataset.data.reduce(function(a, b) {
+							return a + b;
+						});
+						return value > 0 ? 20 : 2;
+					},
+				},
+				point: {
+					radius: 10,
+				}
+			},
+			scales: {
+				xAxes: [{display: false}],
+				yAxes: [
+					{
+						display: false,
+						ticks: {
+							beginAtZero: true
+						}
+					}
+				]
+			}
+		}
+	},
+	options: {
+		canvas: {
+			height: 256,
+			width: 512
+		}
+	}
+};
