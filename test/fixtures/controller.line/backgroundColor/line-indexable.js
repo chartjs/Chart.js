fc9a2d7cd459794ac987f3aa67443e44348e281d
Janelle de Ment <janelledement@gmail.com>
2019-03-07 11:43:51 -0400

    Slight change to line-indexable backgroundColor.

--- test/fixtures/controller.line/backgroundColor/line-indexable.js
+++ test/fixtures/controller.line/backgroundColor/line-indexable.js
@@ -5,9 +5,14 @@ module.exports = {
 			labels: [0, 1, 2, 3, 4, 5],
 			datasets: [
 				{
+					// option in dataset
 					data: [0, 5, 10, null, -10, -5],
+					backgroundColor: [
+						'#ff88ff',
+					]
 				},
 				{
+					// option in element (fallback)
 					data: [4, -5, -10, null, 10, 5],
 				}
 			]
