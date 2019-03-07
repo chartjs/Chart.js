d9732bc288167b5489300b4e45f2ba2d8c16da00
Janelle de Ment <janelledement@gmail.com>
2019-03-07 11:47:46 -0400

    Adds tests for borderColor

---
+++ test/fixtures/controller.line/borderColor/line-scriptable.js
@@ -0,0 +1,63 @@
+module.exports = {
+	config: {
+		type: 'line',
+		data: {
+			labels: [0, 1, 2, 3, 4, 5],
+			datasets: [
+				{
+					// option in dataset
+					data: [4, 5, 10, null, -10, -5],
+					borderColor: function(ctx) {
+						var dataTotal = ctx.dataset.data.reduce(function(a, b) {
+							return a + b;
+						});
+						var c = dataTotal > 0 ? '#D60000' : '#44DE28';
+
+						return c;
+					}
+				},
+				{
+					// option in element (fallback)
+					data: [-4, -5, -10, null, 10, 5],
+				}
+			]
+		},
+		options: {
+			legend: false,
+			title: false,
+			elements: {
+				line: {
+					fill: false,
+					borderColor: function(ctx) {
+						var dataTotal = ctx.dataset.data.reduce(function(a, b) {
+							return a + b;
+						});
+						var c = dataTotal > 0 ? '#D60000' : '#44DE28';
+
+						return c;
+					},
+				},
+				point: {
+					radius: 10,
+				}
+			},
+			scales: {
+				xAxes: [{display: false}],
+				yAxes: [
+					{
+						display: false,
+						ticks: {
+							beginAtZero: true
+						}
+					}
+				]
+			}
+		}
+	},
+	options: {
+		canvas: {
+			height: 256,
+			width: 512
+		}
+	}
+};
