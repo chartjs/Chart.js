fd338d7a0e48039b3b1cd219d64d18474ba48c2b
Janelle de Ment <janelledement@gmail.com>
2019-03-17 10:32:18 -0400

    Improves clarity of borderCapStyle tests/pngs.

--- test/fixtures/controller.line/borderCapStyle/scriptable.js
+++ test/fixtures/controller.line/borderCapStyle/scriptable.js
@@ -2,23 +2,25 @@ module.exports = {
 	config: {
 		type: 'line',
 		data: {
-			labels: [0, 1, 2, 3, 4, 5],
+			labels: [0, 1, 2, 3, 4],
 			datasets: [
 				{
 					// option in dataset
-					data: [4, 5, 10, null, -10, -5],
+					data: [4, null, 3, 3],
 					borderCapStyle: function(ctx) {
-						var dataTotal = ctx.dataset.data.reduce(function(a, b) {
-							return a + b;
-						});
-						var style = dataTotal > 0 ? 'round' : 'square';
-
-						return style;
+						var index = (ctx.datasetIndex % 2);
+						return index === 0 ? 'round'
+							: index === 1 ? 'square'
+							: 'butt'
 					}
 				},
 				{
 					// option in element (fallback)
-					data: [-4, -5, -10, null, 10, 5],
+					data: [4, null, 2, 2],
+				},
+				{
+					// option in element (fallback)
+					data: [4, null, 1, 1],
 				}
 			]
 		},
@@ -28,18 +30,22 @@ module.exports = {
 			elements: {
 				line: {
 					borderColor: '#D60000',
-					borderWidth: 10,
+					borderWidth: 32,
 					borderCapStyle: function(ctx) {
-						var dataTotal = ctx.dataset.data.reduce(function(a, b) {
-							return a + b;
-						});
-						var style = dataTotal > 0 ? 'round' : 'square';
-
-						return style;
+						var index = (ctx.datasetIndex % 3);
+						return index === 0 ? 'round'
+							: index === 1 ? 'square'
+							: 'butt'
 					}
 				},
 				point: {
 					radius: 10,
+				}
+			},
+			layout: {
+				padding: {
+					left: 40,
+					right: 40
 				}
 			},
 			scales: {
--- test/fixtures/controller.line/borderCapStyle/value.js
+++ test/fixtures/controller.line/borderCapStyle/value.js
@@ -2,16 +2,21 @@ module.exports = {
 	config: {
 		type: 'line',
 		data: {
-			labels: [0, 1, 2, 3, 4, 5],
+			labels: [0, 1, 2, 3, 4],
 			datasets: [
 				{
 					// option in dataset
-					data: [0, 5, 10, null, -10, -5],
+					data: [4, null, 3, 3],
 					borderCapStyle: 'round',
 				},
 				{
+					// option in dataset
+					data: [4, null, 2, 2],
+					borderCapStyle: 'square',
+				},
+				{
 					// option in element (fallback)
-					data: [4, -5, -10, null, 10, 5],
+					data: [0, null, 1, 1],
 				}
 			]
 		},
@@ -22,11 +27,17 @@ module.exports = {
 				line: {
 					borderColor: '#00ff00',
 					borderCapStyle: 'butt',
-					borderWidth: 10,
+					borderWidth: 32,
 					fill: false,
 				},
 				point: {
 					radius: 10,
+				}
+			},
+			layout: {
+				padding: {
+					left: 40,
+					right: 40
 				}
 			},
 			scales: {
