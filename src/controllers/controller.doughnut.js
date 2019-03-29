d9b24b46d97360be145a931422003c1cfc51fdd7
Ian Del Giudice <ian.delgiudice@10up.com>
2019-03-29 19:14:48 -0400

    replace HTML string array with DOM node creation for doughnut and polarArea legendCallback

--- src/controllers/controller.doughnut.js
+++ src/controllers/controller.doughnut.js
@@ -22,8 +22,8 @@ defaults._set('doughnut', {
 		mode: 'single'
 	},
 	legendCallback: function(chart) {
-		var text = [];
-		text.push('<ul class="' + chart.id + '-legend">');
+		var text = document.createElement('ul');
+		text.setAttribute('class', chart.id + '-legend');
 
 		var data = chart.data;
 		var datasets = data.datasets;
@@ -31,16 +31,16 @@ defaults._set('doughnut', {
 
 		if (datasets.length) {
 			for (var i = 0; i < datasets[0].data.length; ++i) {
-				text.push('<li><span style="background-color:' + datasets[0].backgroundColor[i] + '"></span>');
+				var listItem = text.appendChild(document.createElement('li'));
+				var listItemSpan = listItem.appendChild(document.createElement('span'));
+				listItemSpan.style.backgroundColor = datasets[0].backgroundColor[i];
 				if (labels[i]) {
-					text.push(labels[i]);
+					listItem.appendChild(document.createTextNode(labels[i]));
 				}
-				text.push('</li>');
 			}
 		}
 
-		text.push('</ul>');
-		return text.join('');
+		return text;
 	},
 	legend: {
 		labels: {
