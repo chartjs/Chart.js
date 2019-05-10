4745b7ddf0f8c7fe95b682eaf9e607ae2b7e6a39
Ian Del Giudice <ian.delgiudice@10up.com>
2019-05-10 16:25:56 -0400

    add check for datasets values in doughnut and polar area

--- src/controllers/controller.doughnut.js
+++ src/controllers/controller.doughnut.js
@@ -22,18 +22,20 @@ defaults._set('doughnut', {
 		var list = document.createElement('ul');
 		var data = chart.data;
 		var datasets = data.datasets;
-		var labels = data.labels;
 
-		var i, ilen, listItem, listItemSpan;
+		if (datasets.length) {
+			var labels = data.labels;
+			var i, ilen, listItem, listItemSpan;
 
-		list.setAttribute('class', chart.id + '-legend');
+			list.setAttribute('class', chart.id + '-legend');
 
-		for (i = 0, ilen = datasets[0].data.length || 0; i < ilen; ++i) {
-			listItem = list.appendChild(document.createElement('li'));
-			listItemSpan = listItem.appendChild(document.createElement('span'));
-			listItemSpan.style.backgroundColor = datasets[0].backgroundColor[i];
-			if (labels[i]) {
-				listItem.appendChild(document.createTextNode(labels[i]));
+			for (i = 0, ilen = datasets[0].data.length; i < ilen; ++i) {
+				listItem = list.appendChild(document.createElement('li'));
+				listItemSpan = listItem.appendChild(document.createElement('span'));
+				listItemSpan.style.backgroundColor = datasets[0].backgroundColor[i];
+				if (labels[i]) {
+					listItem.appendChild(document.createTextNode(labels[i]));
+				}
 			}
 		}
 
