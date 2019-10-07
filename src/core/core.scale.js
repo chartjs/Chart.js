2c7df51555a4318ce70f6efacfa9d02fe61b6596
Eliz <elizkilic@gmail.com>
2019-10-07 15:50:26 +0300

    Update ternary operation

--- src/core/core.scale.js
+++ src/core/core.scale.js
@@ -1237,11 +1237,11 @@ var Scale = Element.extend({
 			switch (scaleLabelAlign) {
 			case 'start':
 				scaleLabelY = me.top + (startsAtBottom ? me.height : 0);
-				textAlign = startsAtBottom ^ !isLeft ? 'left' : 'right';
+				textAlign = startsAtBottom === isLeft ? 'left' : 'right';
 				break;
 			case 'end':
 				scaleLabelY = me.top + (startsAtBottom ? 0 : me.height);
-				textAlign = startsAtBottom ^ !isLeft ? 'right' : 'left';
+				textAlign = startsAtBottom === isLeft ? 'right' : 'left';
 				break;
 			default:
 				scaleLabelY = me.top + me.height / 2;
