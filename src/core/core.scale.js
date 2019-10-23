3cb308d16caddfce8a50a4e616149a3c05f0e50a
Ben McCann <322311+benmccann@users.noreply.github.com>
2019-10-22 17:59:22 -0700

    Fix sampleSize option (#6586)

--- src/core/core.scale.js
+++ src/core/core.scale.js
@@ -448,7 +448,7 @@ var Scale = Element.extend({
 
 		// Compute tick rotation and fit using a sampled subset of labels
 		// We generally don't need to compute the size of every single label for determining scale size
-		samplingEnabled = sampleSize > ticks.length;
+		samplingEnabled = sampleSize < ticks.length;
 		labels = me._convertTicksToLabels(samplingEnabled ? sample(ticks, sampleSize) : ticks);
 
 		// _configure is called twice, once here, once from core.controller.updateLayout.
