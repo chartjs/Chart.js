30cbdc6a4d47e06b20f70534b2bccc46118d1401
Jukka Kurkela <jukka.kurkela@gmail.com>
2019-11-12 20:59:29 +0200

    Convert element.line mock tests to fixtures (#6730)

---
+++ test/fixtures/element.line/stepped/middle.js
@@ -0,0 +1,30 @@
+module.exports = {
+	config: {
+		type: 'line',
+		data: {
+			datasets: [
+				{
+					data: [{x: 1, y: 10}, {x: 5, y: 0}, {x: 15, y: -10}, {x: 19, y: -5}],
+					borderColor: 'red',
+					fill: false,
+					lineTension: 0,
+					steppedLine: 'middle'
+				}
+			]
+		},
+		options: {
+			legend: false,
+			title: false,
+			scales: {
+				xAxes: [{type: 'linear', display: false, ticks: {min: 0, max: 20}}],
+				yAxes: [{display: false, ticks: {min: -15, max: 15}}]
+			}
+		}
+	},
+	options: {
+		canvas: {
+			height: 256,
+			width: 512
+		}
+	}
+};
