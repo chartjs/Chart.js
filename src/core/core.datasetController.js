9090b53410e18ca91b79341e24f76633a56589a5
Ben McCann <322311+benmccann@users.noreply.github.com>
2019-12-13 20:52:18 -0800

    Make elements optional (#6829)

--- src/core/core.datasetController.js
+++ src/core/core.datasetController.js
@@ -624,7 +624,7 @@ helpers.extend(DatasetController.prototype, {
 		const chart = this.chart;
 		const meta = this._cachedMeta;
 		const metaData = meta.data;
-		const ilen = metaData.length;
+		const ilen = meta._parsed.length;
 		const stacked = canStack && meta._stacked;
 		const indices = getSortedDatasetIndices(chart, true);
 		const otherScale = this._getOtherScale(scale);
@@ -639,7 +639,7 @@ helpers.extend(DatasetController.prototype, {
 			parsed = meta._parsed[i];
 			value = parsed[scale.id];
 			otherValue = parsed[otherScale.id];
-			if (item.hidden || isNaN(value) ||
+			if ((item && item.hidden) || isNaN(value) ||
 				otherMin > otherValue || otherMax < otherValue) {
 				continue;
 			}
