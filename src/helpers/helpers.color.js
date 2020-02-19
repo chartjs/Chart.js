b26ce1cefd7a019af5200e31ffe16bfa2e9caf6b
Jukka Kurkela <jukka.kurkela@gmail.com>
2020-02-19 21:44:41 +0200

    Fix typo

--- src/helpers/helpers.color.js
+++ src/helpers/helpers.color.js
@@ -3,14 +3,14 @@ import colorLib from '@kurkle/color';
 /**
  * @param {string | CanvasGradient | CanvasPattern} value
  */
-const isPatternOrGradiend = (value) => value instanceof CanvasGradient || value instanceof CanvasPattern;
+const isPatternOrGradient = (value) => value instanceof CanvasGradient || value instanceof CanvasPattern;
 
 /**
  * @param {string|CanvasGradient|CanvasPattern} value
  * @return {CanvasGradient|CanvasPattern|colorLib}
  */
 export function color(value) {
-	return isPatternOrGradiend(value) ? value : colorLib(value);
+	return isPatternOrGradient(value) ? value : colorLib(value);
 }
 
 /**
@@ -18,7 +18,7 @@ export function color(value) {
  * @return {string|CanvasGradient|CanvasPattern}
  */
 export function getHoverColor(value) {
-	return isPatternOrGradiend(value)
+	return isPatternOrGradient(value)
 		? value
 		: colorLib(value).saturate(0.5).darken(0.1).hexString();
 }
