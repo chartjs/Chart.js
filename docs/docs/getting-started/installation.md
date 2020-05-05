2851a94b9206a37a238f91a8d364f75df17d4d14
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-05-05 12:19:57 -0700

    Migrate from GitBook to Docusaurus (#7295)
    Migrate from GitBook to Docusaurus

---
+++ docs/docs/getting-started/installation.md
@@ -0,0 +1,34 @@
+---
+title: Installation
+---
+
+## npm
+[![npm](https://img.shields.io/npm/v/chart.js.svg?style=flat-square&maxAge=600)](https://npmjs.com/package/chart.js)
+[![npm](https://img.shields.io/npm/dm/chart.js.svg?style=flat-square&maxAge=600)](https://npmjs.com/package/chart.js)
+
+```sh
+npm install chart.js 
+```
+
+## CDN
+
+### CDNJS
+[![cdnjs](https://img.shields.io/cdnjs/v/Chart.js.svg?style=flat-square&maxAge=600)](https://cdnjs.com/libraries/Chart.js)
+
+Chart.js built files are available on [CDNJS](https://cdnjs.com/):
+
+https://cdnjs.com/libraries/Chart.js
+
+### jsDelivr
+[![jsdelivr](https://img.shields.io/npm/v/chart.js.svg?label=jsdelivr&style=flat-square&maxAge=600)](https://cdn.jsdelivr.net/npm/chart.js@latest/dist/) [![jsdelivr hits](https://data.jsdelivr.com/v1/package/npm/chart.js/badge)](https://www.jsdelivr.com/package/npm/chart.js)
+
+Chart.js built files are also available through [jsDelivr](https://www.jsdelivr.com/):
+
+https://www.jsdelivr.com/package/npm/chart.js?path=dist
+
+## Github
+[![github](https://img.shields.io/github/release/chartjs/Chart.js.svg?style=flat-square&maxAge=600)](https://github.com/chartjs/Chart.js/releases/latest)
+
+You can download the latest version of [Chart.js on GitHub](https://github.com/chartjs/Chart.js/releases/latest).
+
+If you download or clone the repository, you must [build](../developers/contributing.md#building-and-testing) Chart.js to generate the dist files. Chart.js no longer comes with prebuilt release versions, so an alternative option to downloading the repo is **strongly** advised.
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,10 @@ set -e
 TARGET_DIR='gh-pages'
 TARGET_BRANCH='master'
 TARGET_REPO_URL="https://$GITHUB_AUTH_TOKEN@github.com/chartjs/chartjs.github.io.git"
-VERSION_REGEX='[[:digit:]]+.[[:digit:]]+.[[:digit:]]+(-.*)?'
 
+# Note: this code also exists in docs-config.sh
 # Make sure that this script is executed only for the release and master branches
+VERSION_REGEX='[[:digit:]]+.[[:digit:]]+.[[:digit:]]+(-.*)?'
 if [[ "$TRAVIS_BRANCH" =~ ^release.*$ ]]; then
     # Travis executes this script from the repository root, so at the same level than package.json
     VERSION=$(node -p -e "require('./package.json').version")
@@ -18,33 +19,47 @@ else
     exit 0
 fi
 
-function update_latest {
-    local out_path=$1
-
-    # The most recent directory whose name is a version number
-    local latest=($(ls -v $out_path | egrep '^('$VERSION_REGEX')$' | tail -1))
-
-    # As soon as a single version is deployed this line is a no-op
-    if [ "$latest" == "" ]; then latest='master'; fi
-
-    # Don't update "latest" on alpha or beta releases
-    if [[ "$latest" =~ ^[^-]+$ ]]; then
-        rm -f $out_path/latest
-        ln -s $latest $out_path/latest
-    fi
-
-    # Always update "next"
-    rm -f $out_path/next
-    ln -s $latest $out_path/next
+function move_sample_scripts {
+    local subdirectory=$1
+    for f in $(find ./samples/$subdirectory -name '*.html'); do
+       sed -i -E "s/((\.\.\/)+dist\/)/..\/\1$subdirectory\//" $f
+    done
 }
 
-function deploy_files {
-    local in_files=$1
-    local out_path=$2
-    rm -rf $out_path/$VERSION
-    mkdir -p $out_path/$VERSION
-    cp -r $in_files $out_path/$VERSION
-    update_latest $out_path
+function update_with_tag {
+    local tag=$1
+    rm -rf "docs/$tag"
+    cp -r ../dist/docs docs/$tag
+    rm -rf "samples/$tag"
+    cp -r ../samples samples/$tag
+
+    move_sample_scripts $tag
+
+    deploy_versioned_files $tag
+}
+
+# Note: this code also exists in docs-config.sh
+# tag is next|latest|master
+# https://www.chartjs.org/docs/$tag/
+# https://www.chartjs.org/samples/$tag/
+# https://www.chartjs.org/dist/$tag/Chart.*js
+function update_tagged_files {
+    if [ "$VERSION" == "master" ]; then
+        update_with_tag master
+    elif [[ "$VERSION" =~ ^[^-]+$ ]]; then
+        update_with_tag lastest
+    else
+        update_with_tag next
+    fi
+}
+
+function deploy_versioned_files {
+    local version=$1
+    local in_files='../dist/Chart*.js'
+    local out_path='./dist'
+    rm -rf $out_path/$version
+    mkdir -p $out_path/$version
+    cp -r $in_files $out_path/$version
 }
 
 # Clone the repository and checkout the gh-pages branch
@@ -52,21 +67,12 @@ git clone $TARGET_REPO_URL $TARGET_DIR
 cd $TARGET_DIR
 git checkout $TARGET_BRANCH
 
-# Copy dist files
-deploy_files '../dist/*.js' './dist'
+# Copy distribution files
+deploy_versioned_files $VERSION
 
-# Copy generated documentation
-deploy_files '../dist/docs/*' './docs'
+update_tagged_files
 
-# Copy samples ...
-deploy_files '../samples/*' './samples'
-
-# ... and relocate samples Chart/js scripts
-for f in $(find ./samples/$VERSION -name '*.html'); do
-   sed -i -E "s/((\.\.\/)+dist\/)/..\/\1$VERSION\//" $f
-done
-
-git add -A
+git add --all
 
 git remote add auth-origin $TARGET_REPO_URL
 git config --global user.email "$GITHUB_AUTH_EMAIL"
