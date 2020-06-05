ed0c41d28d28b569b75014711267eb9e0547247e
AbdelfattahToulaoui <aft.power.2014@gmail.com>
2020-06-05 23:35:26 +0100

    Implemented automatic color generation

--- src/helpers/helpers.color.js
+++ src/helpers/helpers.color.js
@@ -1,5 +1,8 @@
 import colorLib from '@kurkle/color';
 
+let hue = 0;
+const VRATE = 137;
+
 const isPatternOrGradient = (value) => value instanceof CanvasGradient || value instanceof CanvasPattern;
 
 export function color(value) {
@@ -10,4 +13,15 @@ export function getHoverColor(value) {
 	return isPatternOrGradient(value)
 		? value
 		: colorLib(value).saturate(0.5).darken(0.1).hexString();
+}
+
+export function generatedColor(index = -1) {
+	let h;
+	if (index < 0) {
+		h = hue;
+		hue = (hue + VRATE) % 360;
+	} else {
+		h = (index * VRATE) % 360;
+	}
+	return color(`hsl(${h}, 80%, 60%)`).rgbString();
 }
