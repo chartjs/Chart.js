074712b397a8c59ba921c704028622533ba40f14
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-06-20 15:56:43 -0700

    Move timeseries fixtures to their own directory

--- test/fixtures/scale.timeseries/source-labels-offset-min-max.js
+++ test/fixtures/scale.timeseries/source-labels-offset-min-max.js
@@ -4,7 +4,7 @@ module.exports = {
 		type: 'line',
 		data: {
 			labels: ['2017', '2019', '2020', '2025', '2042'],
-			datasets: [{data: [0, 1, 2, 3, 4, 5], fill: false}]
+			datasets: [{data: [0, 1, 2, 3, 4], fill: false}]
 		},
 		options: {
 			scales: {
