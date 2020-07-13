769502316cc784b90a0f45b8d2e1404c93e26783
kurkle <jukka.kurkela@gmail.com>
2020-07-13 12:16:35 +0300

    Move showLine-tests as fixtures

---
+++ test/fixtures/controller.line/showLines/dataset.js
@@ -0,0 +1,36 @@
+module.exports = {
+	description: 'should draw all elements except lines turned off per dataset',
+	config: {
+		type: 'line',
+		data: {
+			datasets: [{
+				data: [10, 15, 0, -4],
+				label: 'dataset1',
+				borderColor: 'red',
+				backgroundColor: 'green',
+				showLine: false,
+				fill: false
+			}],
+			labels: ['label1', 'label2', 'label3', 'label4']
+		},
+		options: {
+			legend: false,
+			title: false,
+			showLines: true,
+			scales: {
+				x: {
+					display: false
+				},
+				y: {
+					display: false
+				}
+			}
+		}
+	},
+	options: {
+		canvas: {
+			width: 512,
+			height: 512
+		}
+	}
+};
