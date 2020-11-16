1b6226342a6425a5926662b127ee17a7809952e6
Jukka Kurkela <jukka.kurkela@gmail.com>
2020-11-16 20:19:59 +0200

    Refactor/cleanup range option handling (#8057)

--- src/scales/scale.linear.js
+++ src/scales/scale.linear.js
@@ -1,4 +1,4 @@
-import {isFinite, valueOrDefault} from '../helpers/helpers.core';
+import {isFinite} from '../helpers/helpers.core';
 import LinearScaleBase from './scale.linearbase';
 import Ticks from '../core/core.ticks';
 
@@ -6,16 +6,10 @@ export default class LinearScale extends LinearScaleBase {
 
 	determineDataLimits() {
 		const me = this;
-		const options = me.options;
 		const {min, max} = me.getMinMax(true);
 
-		me.min = isFinite(min) ? min : valueOrDefault(options.suggestedMin, 0);
-		me.max = isFinite(max) ? max : valueOrDefault(options.suggestedMax, 1);
-
-		// Backward compatible inconsistent min for stacked
-		if (options.stacked && min > 0) {
-			me.min = 0;
-		}
+		me.min = isFinite(min) ? min : 0;
+		me.max = isFinite(max) ? max : 1;
 
 		// Common base implementation to handle min, max, beginAtZero
 		me.handleTickRangeOptions();
@@ -37,8 +31,7 @@ export default class LinearScale extends LinearScaleBase {
 
 	// Utils
 	getPixelForValue(value) {
-		const me = this;
-		return me.getPixelForDecimal((value - me._startValue) / me._valueRange);
+		return this.getPixelForDecimal((value - this._startValue) / this._valueRange);
 	}
 
 	getValueForPixel(pixel) {
