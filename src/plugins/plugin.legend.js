51d0444fa6977411bbd06de04dd1ab53f3070e9c
Jacco van den Berg <jaccoberg2281@gmail.com>
2020-12-05 15:45:52 +0100

    fix default font color for legend

--- src/plugins/plugin.legend.js
+++ src/plugins/plugin.legend.js
@@ -311,7 +311,7 @@ export class Legend extends Element {
 		const rtlHelper = getRtlAdapter(opts.rtl, me.left, me._minSize.width);
 		const ctx = me.ctx;
 		const labelFont = toFont(labelOpts.font, me.chart.options.font);
-		const fontColor = labelOpts.color;
+		const fontColor = labelOpts.color || defaultColor;
 		const fontSize = labelFont.size;
 		let cursor;
 
