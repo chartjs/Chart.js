649f8153281bf5dfa17a02d8d7923b3e699177a0
Evert Timberg <evert.timberg+github@gmail.com>
2020-12-06 18:17:22 -0500

    Respect the set locale for in the pie/doughnut tooltips (#8142)

--- src/controllers/controller.doughnut.js
+++ src/controllers/controller.doughnut.js
@@ -235,10 +235,11 @@ export default class DoughnutController extends DatasetController {
 		const meta = me._cachedMeta;
 		const chart = me.chart;
 		const labels = chart.data.labels || [];
+		const value = new Intl.NumberFormat(chart.options.locale).format(meta._parsed[index]);
 
 		return {
 			label: labels[index] || '',
-			value: meta._parsed[index],
+			value,
 		};
 	}
 
