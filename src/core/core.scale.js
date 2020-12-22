36eabe4cde4af533b1d77a9bef6ccfca2bbfd395
Evert Timberg <evert.timberg@gmail.com>
2020-12-22 10:53:49 -0500

    Linting

--- src/core/core.scale.js
+++ src/core/core.scale.js
@@ -1390,6 +1390,7 @@ export default class Scale extends Element {
 						textOffset = (-1 * labelSizes.highest.height) + (0.5 * lineHeight);
 					}
 				} else {
+					// eslint-disable-next-line no-lonely-if
 					if (crossAlign === 'near' || rotation !== 0) {
 						textOffset = Math.sin(rotation) * halfCount * lineHeight;
 						textOffset += (rotation === 0 ? 0.5 : Math.cos(rotation) * halfCount) * lineHeight;
