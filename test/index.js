75c2fca33d36146bb3137ec5a3bed4f7e2f0ff2b
Jukka Kurkela <jukka.kurkela@gmail.com>
2020-12-27 20:46:46 +0200

    Migrate to chartjs-test-utils

--- test/index.js
+++ test/index.js
@@ -1,63 +1,27 @@
-import fixture from './fixture';
-import Context from './context';
-import matchers from './matchers';
-import utils from './utils';
+import {acquireChart, releaseChart, createMockContext, afterEvent, waitForResize, injectWrapperCSS, specsFromFixtures, triggerMouseEvent, addMatchers, releaseCharts} from 'chartjs-test-utils';
 
-(function() {
+// force ratio=1 for tests on high-res/retina devices
+// fixes https://github.com/chartjs/Chart.js/issues/4515
+window.devicePixelRatio = 1;
 
-	// Keep track of all acquired charts to automatically release them after each specs
-	var charts = {};
+window.acquireChart = acquireChart;
+window.afterEvent = afterEvent;
+window.releaseChart = releaseChart;
+window.waitForResize = waitForResize;
+window.createMockContext = createMockContext;
 
-	function acquireChart() {
-		var chart = utils.acquireChart.apply(utils, arguments);
-		charts[chart.id] = chart;
-		return chart;
-	}
+injectWrapperCSS();
 
-	function releaseChart(chart) {
-		utils.releaseChart.apply(utils, arguments);
-		delete charts[chart.id];
-	}
+jasmine.fixture = {
+	specs: specsFromFixtures
+};
 
-	function createMockContext() {
-		return new Context();
-	}
+jasmine.triggerMouseEvent = triggerMouseEvent;
 
-	// force ratio=1 for tests on high-res/retina devices
-	// fixes https://github.com/chartjs/Chart.js/issues/4515
-	window.devicePixelRatio = 1;
+beforeEach(function() {
+	addMatchers();
+});
 
-	window.acquireChart = acquireChart;
-	window.afterEvent = utils.afterEvent;
-	window.releaseChart = releaseChart;
-	window.waitForResize = utils.waitForResize;
-	window.createMockContext = createMockContext;
-
-	// some style initialization to limit differences between browsers across different platforms.
-	utils.injectCSS(
-		'.chartjs-wrapper, .chartjs-wrapper canvas {' +
-			'border: 0;' +
-			'margin: 0;' +
-			'padding: 0;' +
-		'}' +
-		'.chartjs-wrapper {' +
-			'position: absolute' +
-		'}');
-
-	jasmine.fixture = fixture;
-	jasmine.triggerMouseEvent = utils.triggerMouseEvent;
-
-	beforeEach(function() {
-		jasmine.addMatchers(matchers);
-	});
-
-	afterEach(function() {
-		// Auto releasing acquired charts
-		Object.keys(charts).forEach(function(id) {
-			var chart = charts[id];
-			if (!(chart.$test || {}).persistent) {
-				releaseChart(chart);
-			}
-		});
-	});
-}());
+afterEach(function() {
+	releaseCharts();
+});
