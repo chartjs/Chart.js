987ef8bf89b7ff710dfa29a5e29c069ef7538027
Evert Timberg <evert.timberg@gmail.com>
2020-12-31 11:58:54 -0500

    Add decimation plugin to the core

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,3 +1,4 @@
+export {default as Decimation} from './plugin.decimation';
 export {default as Filler} from './plugin.filler';
 export {default as Legend} from './plugin.legend';
 export {default as Title} from './plugin.title';
---
+++ src/plugins/plugin.decimation.js
@@ -0,0 +1,83 @@
+export default {
+	id: 'decimation',
+
+	beforeElementsUpdate: (chart, args, options) => {
+		if (!options.enabled) {
+			return;
+		}
+
+		// Use an approximation since this is before scales are parsed
+		const verticalAxisCount = Object.values(chart.scales).reduce((acc, scale) => acc + scale.axis === 'y' ? 1 : 0, 0);
+
+		// assume ~50px for now. If the axis is actually wider then we have more points than needed
+		// TODO: Take display settings into account and compute a more accurate guess for each axis
+		const availableWidth = chart.width - (verticalAxisCount * 50);
+
+		chart.data.datasets.forEach((dataset, datasetIndex) => {
+			let i, point, x, y, prevX, minIndex, maxIndex, minY, maxY;
+			const {data} = dataset;
+			const meta = chart.getDatasetMeta(datasetIndex);
+
+			if (meta.type !== 'line') {
+				// Only line datasets are supported
+				return;
+			}
+
+			const xAxis = chart.scales[meta.xAxisID];
+			if (xAxis.type !== 'linear' && xAxis.type !== 'time') {
+				// Only linear interpolation is supported
+				return;
+			}
+
+			if (chart.options.parsing) {
+				// Plugin only supports data that does not need parsing
+				return;
+			}
+
+			if (data.length <= 4 * availableWidth) {
+				// No decimation is required until we are above this threshold
+				return;
+			}
+
+			const decimated = [];
+
+			const xMin = data[0].x;
+			const xMax = data[data.length - 1].x;
+			const dx = xMax - xMin;
+
+			for (i = 0; i < data.length; ++i) {
+				point = data[i];
+				x = (point.x - xMin) / dx * availableWidth;
+				y = point.y;
+				const truncX = x | 0;
+
+				if (truncX === prevX) {
+					// Determine `minY` / `maxY` and `avgX` while we stay within same x-position
+					if (y < minY) {
+						minY = y;
+						minIndex = i;
+					} else if (y > maxY) {
+						maxY = y;
+						maxIndex = i;
+					}
+				} else {
+					// Push up to 4 points, 3 for the last interval and the first point for this interval
+					if (minIndex && maxIndex) {
+						decimated.push(data[minIndex], data[maxIndex]);
+					}
+					if (i > 0) {
+						// Last point in the previous interval
+						decimated.push(data[i - 1]);
+					}
+					decimated.push(point);
+					prevX = truncX;
+					minY = maxY = y;
+					minIndex = maxIndex = i;
+				}
+			}
+
+			dataset._data = dataset.data;
+			dataset.data = decimated;
+		});
+	}
+};
