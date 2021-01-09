47780b433a7e6492f0c62a8c69d1c8660524da70
Jacco van den Berg <jaccoberg2281@gmail.com>
2021-01-09 01:05:37 +0100

    added test for functionality

--- test/specs/core.datasetController.tests.js
+++ test/specs/core.datasetController.tests.js
@@ -38,6 +38,51 @@ describe('Chart.DatasetController', function() {
 		});
 	});
 
+	it('should not try to delete non existent stacks', function() {
+		function createAndUpdateChart() {
+			var chart = acquireChart({
+				data: {
+					labels: ['q'],
+					datasets: [
+						{
+							id: 'dismissed',
+							label: 'Test before',
+							yAxisID: 'count',
+							data: [816],
+							type: 'bar',
+							stack: 'stack'
+						}
+					]
+				},
+				options: {
+					scales: {
+						count: {
+							axis: 'y',
+							type: 'linear'
+						}
+					}
+				}
+			});
+
+			chart.data = {
+				datasets: [
+					{
+						id: 'tests',
+						yAxisID: 'count',
+						label: 'Test after',
+						data: [38300],
+						type: 'bar'
+					}
+				],
+				labels: ['q']
+			};
+
+			chart.update();
+		}
+
+		expect(createAndUpdateChart).not.toThrow();
+	});
+
 	describe('inextensible data', function() {
 		it('should handle a frozen data object', function() {
 			function createChart() {
