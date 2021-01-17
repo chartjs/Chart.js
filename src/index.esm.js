6915ce0ac117f0dba4f8054fca58848349cbe9d2
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-01-17 15:20:58 -0800

    Add default export to esm build to make date adapters work

--- src/index.esm.js
+++ src/index.esm.js
@@ -1,6 +1,10 @@
+import Chart from './core/core.controller';
+
 export * from './controllers';
 export * from './core';
 export * from './elements';
 export * from './platform';
 export * from './plugins';
 export * from './scales';
+
+export default Chart;
