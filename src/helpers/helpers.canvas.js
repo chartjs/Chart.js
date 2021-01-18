b4631b520da7b8da7cd0f91f739248dae660c1ba
Marcel Samyn <marcel.samyn@lab900.com>
2021-01-18 14:07:23 +0100

    fix canvas clearRect size calculation

--- src/helpers/helpers.canvas.js
+++ src/helpers/helpers.canvas.js
@@ -113,7 +113,13 @@ export function _alignPixel(chart, pixel, width) {
  */
 export function clearCanvas(canvas, ctx) {
 	ctx = ctx || canvas.getContext('2d');
+
+	ctx.save();
+	// canvas.width and canvas.height do not consider the canvas transform,
+	// while clearRect does
+	ctx.resetTransform();
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.restore();
 }
 
 export function drawPoint(ctx, options, x, y) {
