de0175d40c1705a8a5ac9a6349449a6da839dc85
Jacco van den Berg <jaccoberg2281@gmail.com>
2021-01-30 14:52:14 +0100

    not working

--- test/specs/controller.scatter.tests.js
+++ test/specs/controller.scatter.tests.js
@@ -31,4 +31,36 @@ describe('Chart.controllers.scatter', function() {
 
 		jasmine.triggerMouseEvent(chart, 'mousemove', point);
 	});
+
+	it('should only show a single point in the tooltip on multiple datasets', function(done) {
+		var chart = window.acquireChart({
+			type: 'scatter',
+			data: {
+				datasets: [{
+					data: [{
+						x: 10,
+						y: 15
+					}],
+					label: 'dataset1'
+				},
+				{
+					data: [{
+						x: 20,
+						y: 10
+					}],
+					label: 'dataset2'
+				}],
+			},
+			options: {}
+		});
+		var point = chart.getDatasetMeta(0).data[0];
+
+		afterEvent(chart, 'mousemove', function() {
+			expect(chart.tooltip.body[0].lines).toEqual(['(10, 15)']);
+
+			done();
+		});
+
+		jasmine.triggerMouseEvent(chart, 'mousemove', point);
+	});
 });
