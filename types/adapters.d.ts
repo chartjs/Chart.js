804b39e9cf6e05037a1299b342d42f36ad1edecc
Jacco van den Berg <jaccoberg2281@gmail.com>
2021-02-06 00:55:06 +0100

    remove declaration. if this is giving problems in the future it might need te be added again

--- types/adapters.d.ts
+++ types/adapters.d.ts
@@ -58,11 +58,6 @@ export interface DateAdapter {
 	endOf(timestamp: number, unit: TimeUnit | 'isoWeek'): number;
 }
 
-declare const DateAdapter: {
-	prototype: DateAdapter;
-	new(options: any): DateAdapter;
-};
-
 export const _adapters: {
 	_date: DateAdapter;
 };
