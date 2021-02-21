9b214c08a5e9cecb3bc3425138af755e3edb6f84
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-02-21 16:13:16 +0200

    Stop defaulting to `r` axis for Scale with id `r` (#8477)

--- src/core/core.config.js
+++ src/core/core.config.js
@@ -34,7 +34,7 @@ function axisFromPosition(position) {
 }
 
 export function determineAxis(id, scaleOptions) {
-  if (id === 'x' || id === 'y' || id === 'r') {
+  if (id === 'x' || id === 'y') {
     return id;
   }
   return scaleOptions.axis || axisFromPosition(scaleOptions.position) || id.charAt(0).toLowerCase();
