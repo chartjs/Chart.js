def8d25d4b7837706c866093d116e75ee84d3f10
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-02-21 16:15:26 +0200

    Fix animation types, add test (#8476)

---
+++ types/tests/animation.ts
@@ -0,0 +1,42 @@
+import { Chart } from '../index.esm';
+
+const chart = new Chart('id', {
+  type: 'bar',
+  data: {
+    labels: [],
+    datasets: [{
+      data: []
+    }]
+  },
+  options: {
+    animation: false,
+    animations: {
+      colors: false,
+      numbers: {
+        properties: ['a', 'b'],
+        type: 'number',
+        from: 0,
+        to: 10,
+        delay: (ctx) => ctx.dataIndex * 100,
+        duration: (ctx) => ctx.datasetIndex * 1000,
+        loop: true,
+        easing: 'linear'
+      }
+    },
+    transitions: {
+      show: {
+        animation: {
+          duration: 10
+        },
+        animations: {
+          numbers: false
+        }
+      },
+      custom: {
+        animation: {
+          duration: 10
+        }
+      }
+    }
+  },
+});
