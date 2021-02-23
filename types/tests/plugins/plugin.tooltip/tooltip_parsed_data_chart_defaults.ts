66a283dc4eb3e111e1ccd537bf22ef442a738c21
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-02-23 17:27:13 +0200

    Add linting of type tests (.ts) + fix errors

--- types/tests/plugins/plugin.tooltip/tooltip_parsed_data_chart_defaults.ts
+++ types/tests/plugins/plugin.tooltip/tooltip_parsed_data_chart_defaults.ts
@@ -1,9 +1,9 @@
 import { Chart } from '../../../index.esm';
 
 Chart.defaults.controllers.bubble.plugins.tooltip.callbacks.label = (item) => {
-    const {x, y, _custom: r} = item.parsed;
-    return `${item.label}: (${x}, ${y}, ${r})`;
-}
+  const { x, y, _custom: r } = item.parsed;
+  return `${item.label}: (${x}, ${y}, ${r})`;
+};
 
 const chart = new Chart('id', {
   type: 'bubble',
