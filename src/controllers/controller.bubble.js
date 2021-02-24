78505bb931dfb8c2ab6bc25291ceff9a4dad4485
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-02-24 06:21:10 -0800

    revert change checking for pixel values

--- src/controllers/controller.bubble.js
+++ src/controllers/controller.bubble.js
@@ -83,7 +83,7 @@ export default class BubbleController extends DatasetController {
       const properties = {
         x,
         y,
-        skip: x === null || y === null
+        skip: isNaN(x) || isNaN(y)
       };
 
       if (includeOptions) {
