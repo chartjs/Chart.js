24b14193b362c59b4d4f809031a5164e34606067
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-03-05 02:37:46 +0200

    Linear: Skip ticks that would overlap with min/max (#8569)

---
+++ test/fixtures/scale.linear/min-max-skip.js
@@ -0,0 +1,20 @@
+module.exports = {
+  description: 'https://github.com/chartjs/Chart.js/issues/7734',
+  config: {
+    type: 'line',
+    options: {
+      scales: {
+        y: {
+          max: 1225.2,
+          min: 369.5,
+        },
+        x: {
+          display: false
+        }
+      }
+    }
+  },
+  options: {
+    spriteText: true
+  }
+};
