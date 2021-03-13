eeb5d8baa4fb8b9d1f4247eee6320dceb1124784
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-03-13 14:19:20 +0200

    Types: fix bubble chart options

---
+++ types/tests/controllers/bubble_chart_options.ts
@@ -0,0 +1,22 @@
+import { Chart, ChartOptions } from '../../index.esm';
+
+const chart = new Chart('test', {
+  type: 'bubble',
+  data: {
+    datasets: []
+  },
+  options: {
+    scales: {
+      x: {
+        min: 0,
+        max: 30,
+        ticks: {}
+      },
+      y: {
+        min: 0,
+        max: 30,
+        ticks: {},
+      },
+    }
+  }
+});
