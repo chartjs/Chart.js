992cdec1b45cab42c7f6943b5024c2e434e934d0
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-03-21 17:27:30 +0200

    Make sure all helpers are exported (#8688)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
+export * from './helpers.color';
 export * from './helpers.core';
 export * from './helpers.canvas';
 export * from './helpers.collection';
@@ -5,12 +6,10 @@ export * from './helpers.config';
 export * from './helpers.curve';
 export * from './helpers.dom';
 export {default as easingEffects} from './helpers.easing';
+export * from './helpers.extras';
 export * from './helpers.interpolation';
 export * from './helpers.intl';
 export * from './helpers.options';
 export * from './helpers.math';
 export * from './helpers.rtl';
 export * from './helpers.segment';
-
-export {color, getHoverColor} from './helpers.color';
-export {requestAnimFrame, fontString} from './helpers.extras';
