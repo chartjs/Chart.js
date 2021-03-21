2111324b77d13428ad93bc62f4fdabe38f4b7cd9
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-03-21 17:30:31 +0200

    Add typings for throttled and debounce

--- types/helpers/helpers.extras.d.ts
+++ types/helpers/helpers.extras.d.ts
@@ -1,6 +1,25 @@
+import { AnyObject } from '../basic';
+
 export function fontString(pixelSize: number, fontStyle: string, fontFamily: string): string;
 
 /**
  * Request animation polyfill
  */
 export function requestAnimFrame(cb: () => void): void;
+
+/**
+ * Throttles calling `fn` once per animation frame
+ * Latest argments are used on the actual call
+ * @param {function} fn
+ * @param {*} thisArg
+ * @param {function} [updateFn]
+ */
+export function throttled(fn: () => void, thisArg: AnyObject, updateFn: (AnyObject) => AnyObject): () => void;
+
+/**
+ * Debounces calling `fn` for `delay` ms
+ * @param {function} fn - Function to call. No arguments are passed.
+ * @param {number} delay - Delay in ms. 0 = immediate invocation.
+ * @returns {function}
+ */
+export function debounce(fn: () => void, delay: number): () => number;
