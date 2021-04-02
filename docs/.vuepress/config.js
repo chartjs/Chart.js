0ef1897a98c8dcd4c88c3332f1a7ec559ef6d920
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-04-02 17:56:32 +0300

    Redirect to first sample (#8783)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,6 +19,12 @@ module.exports = {
         'ga': 'UA-28909194-3'
       }
     ],
+    ['redirect', {
+      redirectors: [
+        // Default sample page when accessing /samples.
+        {base: '/samples', alternative: ['bar/vertical']},
+      ],
+    }],
     [
       'vuepress-plugin-typedoc',
       {
@@ -81,7 +87,6 @@ module.exports = {
         title: 'API'
       },
       '/samples/': [
-        '',
         {
           title: 'Bar Charts',
           children: [
