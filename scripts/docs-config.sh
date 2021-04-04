c6d43bbbc55b8aedbb5cd040f7dea06f6fd9e7b8
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-04-04 16:10:29 +0300

    Update release process to create versioned docs (#8817)
    * Update release process to create versioned docs
    * Remove test, separate tag

--- scripts/docs-config.sh
+++ scripts/docs-config.sh
@@ -2,21 +2,11 @@
 
 set -e
 
+source utils.sh
+
 VERSION=$1
+MODE=$2
 
-# Note: this code also exists in deploy.sh
-# tag is next|latest|master
-# https://www.chartjs.org/docs/$tag/
-function update_config {
-    local tag=''
-    if [ "$VERSION" == "master" ]; then
-        tag=master
-    elif [[ "$VERSION" =~ ^[^-]+$ ]]; then
-        tag=latest
-    else
-        tag=next
-    fi
-    sed -i -e "s/VERSION/$tag/g" "docs/.vuepress/config.js"
-}
+TAG=$(tag_from_version "$VERSION" "$MODE")
 
-update_config
+sed -i -e "s/VERSION/$TAG/g" "docs/.vuepress/config.js"
