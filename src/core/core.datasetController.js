271854be2d7aaaca766ff402b69d9422818509cf
Evert Timberg <evert.timberg@gmail.com>
2021-04-18 15:32:40 -0400

    Simpler implementation of getLastIndexInStack

--- src/core/core.datasetController.js
+++ src/core/core.datasetController.js
@@ -127,25 +127,15 @@ function getOrCreateStack(stacks, stackKey, indexValue) {
   return subStack[indexValue] || (subStack[indexValue] = {});
 }
 
-function getLastIndexInStack(stack, chart, positive) {
-  const items = Object.entries(stack).filter(
-    ([key, v]) => key !== '_top' && key !== '_bottom'
-  ).map(
-    // Make datasetIndex keys into ints
-    ([k, v]) => [parseInt(k, 10), v]
-  ).filter(
-    ([dsIndex, value]) => positive ? value > 0 : value < 0
-  ).sort((a, b) => {
-    const ds1 = chart.data.datasets[a[0]];
-    const ds2 = chart.data.datasets[b[0]];
+function getLastIndexInStack(stack, vScale, positive) {
+  for (const meta of vScale.getMatchingVisibleMetas('bar').reverse()) { 
+    const value = stack[meta.index]; 
+    if ((positive && value > 0) || (!positive && value < 0)) { 
+      return meta.index; 
+    } 
+  }
 
-    // Sort in descending order so that the first item
-    // is the one drawn last
-    return ds1.order === ds2.order
-      ? b[0] - a[0]
-      : ds2.order - ds1.order;
-  });
-  return items.length ? items[0][0] : null;
+  return null;
 }
 
 function updateStacks(controller, parsed) {
@@ -165,8 +155,8 @@ function updateStacks(controller, parsed) {
     stack = itemStacks[vAxis] = getOrCreateStack(stacks, key, index);
     stack[datasetIndex] = value;
 
-    stack._top = getLastIndexInStack(stack, chart, true);
-    stack._bottom = getLastIndexInStack(stack, chart, false);
+    stack._top = getLastIndexInStack(stack, vScale, true);
+    stack._bottom = getLastIndexInStack(stack, vScale, false);
   }
 }
 
