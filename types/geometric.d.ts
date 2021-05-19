597351a2f80031140ceb5fe72de9bc097dedef57
Jon Rimmer <jon.rimmer@gmail.com>
2021-05-19 09:50:01 +0100

    Add missing props to ChartArea type

--- types/geometric.d.ts
+++ types/geometric.d.ts
@@ -1,11 +1,13 @@
 export interface ChartArea {
-	top: number;
-	left: number;
-	right: number;
-	bottom: number;
+  top: number;
+  left: number;
+  right: number;
+  bottom: number;
+  width: number;
+  height: number;
 }
 
 export interface Point {
-	x: number;
-	y: number;
+  x: number;
+  y: number;
 }
