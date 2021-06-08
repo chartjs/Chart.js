1cf95e960d93274cd0c1c735e6c2ad390aef670a
Sergej Sintschilin <seregpie@gmail.com>
2021-06-08 14:52:06 +0200

    export helpers

--- src/index.esm.js
+++ src/index.esm.js
@@ -7,12 +7,14 @@ export * from './scales';
 
 import * as controllers from './controllers';
 import * as elements from './elements';
+import * as helpers from './helpers';
 import * as plugins from './plugins';
 import * as scales from './scales';
 
 export {
   controllers,
   elements,
+  helpers,
   plugins,
   scales,
 };
