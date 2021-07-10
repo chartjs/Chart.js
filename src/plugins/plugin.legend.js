27b91b7458d34caabf5ed50cb08ff2f5f4c6dd1e
Evert Timberg <evert.timberg+github@gmail.com>
2021-07-10 18:36:46 -0400

    Adjust legend hitboxes when RTL and each item has a different size (#9353)
    * Adjust legend hitboxes when RTL and
    each item has a different size
    
    * Add test for RTL legend hitboxes

--- src/plugins/plugin.legend.js
+++ src/plugins/plugin.legend.js
@@ -234,6 +234,7 @@ export class Legend extends Element {
     }
     const titleHeight = me._computeTitleHeight();
     const {legendHitBoxes: hitboxes, options: {align, labels: {padding}, rtl}} = me;
+    const rtlHelper = getRtlAdapter(rtl, me.left, me.width);
     if (this.isHorizontal()) {
       let row = 0;
       let left = _alignStartEnd(align, me.left + padding, me.right - me.lineWidths[row]);
@@ -243,27 +244,8 @@ export class Legend extends Element {
           left = _alignStartEnd(align, me.left + padding, me.right - me.lineWidths[row]);
         }
         hitbox.top += me.top + titleHeight + padding;
-        hitbox.left = left;
+        hitbox.left = rtlHelper.leftForLtr(rtlHelper.x(left), hitbox.width);
         left += hitbox.width + padding;
-      }
-
-      if (rtl) {
-        // When the legend is in RTL mode, each row starts at the right
-        // To ensure that click handling works correctly, we need to ensure that the items in the
-        // hitboxes array line up with how the legend items are drawn (this hack is required until V4)
-        const boxMap = hitboxes.reduce((map, box) => {
-          map[box.row] = map[box.row] || [];
-          map[box.row].push(box);
-          return map;
-        }, {});
-
-        const newBoxes = [];
-        Object.keys(boxMap).forEach(key => {
-          boxMap[key].reverse();
-          newBoxes.push(...boxMap[key]);
-        });
-
-        me.legendHitBoxes = newBoxes;
       }
     } else {
       let col = 0;
@@ -275,6 +257,7 @@ export class Legend extends Element {
         }
         hitbox.top = top;
         hitbox.left += me.left + padding;
+        hitbox.left = rtlHelper.leftForLtr(rtlHelper.x(hitbox.left), hitbox.width);
         top += hitbox.height + padding;
       }
     }
