28651bbc68137986d755054511eae4e75fab088e
Evert Timberg <evert.timberg@gmail.com>
2021-07-13 19:50:04 -0400

    Handle center position for vertical axes

--- src/core/core.scale.js
+++ src/core/core.scale.js
@@ -137,6 +137,7 @@ function titleArgs(scale, offset, position, align) {
   let rotation = 0;
   let maxWidth, titleX, titleY;
   const height = bottom - top;
+  const width = right - left;
 
   if (scale.isHorizontal()) {
     titleX = _alignStartEnd(align, left, right);
@@ -148,11 +149,19 @@ function titleArgs(scale, offset, position, align) {
     } else if (position === 'center') {
       titleY = (chartArea.bottom + chartArea.top) / 2 + height - offset;
     } else {
-    titleY = offsetFromEdge(scale, position, offset);
+      titleY = offsetFromEdge(scale, position, offset);
     }
     maxWidth = right - left;
   } else {
-    titleX = offsetFromEdge(scale, position, offset);
+    if (isObject(position)) {
+      const positionAxisID = Object.keys(position)[0];
+      const value = position[positionAxisID];
+      titleX = scales[positionAxisID].getPixelForValue(value) - width + offset;
+    } else if (position === 'center') {
+      titleX = (chartArea.left + chartArea.right) / 2 - width + offset;
+    } else {
+      titleX = offsetFromEdge(scale, position, offset);
+    }
     titleY = _alignStartEnd(align, bottom, top);
     rotation = position === 'left' ? -HALF_PI : HALF_PI;
   }
