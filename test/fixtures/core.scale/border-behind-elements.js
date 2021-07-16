f2931eede26763efcf1ec3ecac7fcffba4ac5cc4
Jacco van den Berg <jaccoberg2281@gmail.com>
2021-07-16 16:23:17 +0200

    lower default value of grid z index so it draws behind chart elements

---
+++ test/fixtures/core.scale/border-behind-elements.js
@@ -0,0 +1,49 @@
+module.exports = {
+  config: {
+    type: 'bubble',
+    data: {
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [{x: 19, y: 3, r: 3}, {x: 2, y: 2, r: 60}],
+          radius: 100,
+          backgroundColor: 'pink'
+        }
+      ]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        }
+      },
+      scales: {
+        y: {
+          ticks: {
+            display: false
+          },
+          grid: {
+            borderColor: 'red',
+            borderWidth: 5
+          }
+        },
+        x: {
+          ticks: {
+            display: false
+          },
+          grid: {
+            borderColor: 'red',
+            borderWidth: 5
+          }
+        }
+      }
+    }
+  },
+
+  options: {
+    canvas: {
+      height: 512,
+      width: 512
+    }
+  }
+};
