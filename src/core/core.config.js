9b3cc40bc5fc99f7a714a8f870c2e661ae63b6ec
kurkle <jukka.kurkela@gmail.com>
2021-09-05 22:03:35 +0300

    Add sanity checks for scale options

--- src/core/core.config.js
+++ src/core/core.config.js
@@ -48,6 +48,12 @@ function mergeScaleConfig(config, options) {
   // First figure out first scale id's per axis.
   Object.keys(configScales).forEach(id => {
     const scaleConf = configScales[id];
+    if (!isObject(scaleConf)) {
+      return console.error(`Invalid scale configuration for scale: ${id}`);
+    }
+    if (scaleConf._proxy) {
+      return console.warn(`Ignoring resolver passed as options for scale: ${id}`);
+    }
     const axis = determineAxis(id, scaleConf);
     const defaultId = getDefaultScaleIDFromAxis(axis, chartIndexAxis);
     const defaultScaleOptions = chartDefaults.scales || {};
