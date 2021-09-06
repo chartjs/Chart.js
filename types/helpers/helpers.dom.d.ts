61e3eca198b531e42313438db1f88191bbeaf04e
aha-oretama <sekine_y_529@msn.com>
2021-09-06 22:14:00 +0900

    fix getRelativePosition type definition (#9610)
    * fix getRelativePosition type definition
    
    * Update helpers.dom.d.ts
    
    Co-authored-by: Jukka Kurkela <jukka.kurkela@gmail.com>

--- types/helpers/helpers.dom.d.ts
+++ types/helpers/helpers.dom.d.ts
@@ -1,6 +1,8 @@
+import { ChartEvent } from '../index.esm';
+
 export function getMaximumSize(node: HTMLElement, width?: number, height?: number, aspectRatio?: number): { width: number, height: number };
 export function getRelativePosition(
-	evt: MouseEvent,
+	evt: MouseEvent | ChartEvent,
 	chart: { readonly canvas: HTMLCanvasElement }
 ): { x: number; y: number };
 export function getStyle(el: HTMLElement, property: string): string;
