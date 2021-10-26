2a97192154d9bcf9d2bdedd26cad57ce0fce43b4
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-10-26 14:49:04 +0300

    New sample: Progressive Line With Easing (#9798)

--- docs/scripts/helpers.js
+++ docs/scripts/helpers.js
@@ -1,4 +1,4 @@
 // Add helpers needed in samples here.
 // Usable through `helpers[name]`.
-export {color, getHoverColor} from '../../dist/helpers.esm';
+export {color, getHoverColor, easingEffects} from '../../dist/helpers.esm';
 
