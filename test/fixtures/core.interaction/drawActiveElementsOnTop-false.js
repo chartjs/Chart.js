817ce1a59af3fbd8eaba8e8b437bda9ab3d6b98f
Jacco van den Berg <jaccoberg2281@gmail.com>
2021-11-29 17:24:21 +0100

    resolve linting errors

--- test/fixtures/core.interaction/drawActiveElementsOnTop-false.js
+++ test/fixtures/core.interaction/drawActiveElementsOnTop-false.js
@@ -1,34 +1,33 @@
 module.exports = {
-    config: {
-      type: 'bubble',
-      data: {
-        datasets: [{
-          data: [
-            { x: 1, y: 1, r: 80 },
-            { x: 1, y: 1, r: 20 }
-          ],
-          drawActiveElementsOnTop: false,
-          backgroundColor: (ctx) => (ctx.dataIndex === 1 ? "red" : "blue"),
-          hoverBackgroundColor: "yellow",
-          hoverRadius: 0,
-        }]
-      },
-      options: {
-        plugins: {
-          tooltip: false,
-          legend: false
-        },
-      }
+  config: {
+    type: 'bubble',
+    data: {
+      datasets: [{
+        data: [
+          {x: 1, y: 1, r: 80},
+          {x: 1, y: 1, r: 20}
+        ],
+        drawActiveElementsOnTop: false,
+        backgroundColor: (ctx) => (ctx.dataIndex === 1 ? 'red' : 'blue'),
+        hoverBackgroundColor: 'yellow',
+        hoverRadius: 0,
+      }]
     },
     options: {
-      canvas: {
-        width: 600,
-        height: 400
+      plugins: {
+        tooltip: false,
+        legend: false
       },
-      async run(chart) {
-        const point = chart.getDatasetMeta(0).data[0];
-        await jasmine.triggerMouseEvent(chart, 'click', {y: point.y, x: point.x + 25});
-      }
     }
-  };
-  
+  },
+  options: {
+    canvas: {
+      width: 600,
+      height: 400
+    },
+    async run(chart) {
+      const point = chart.getDatasetMeta(0).data[0];
+      await jasmine.triggerMouseEvent(chart, 'click', {y: point.y, x: point.x + 25});
+    }
+  }
+};
