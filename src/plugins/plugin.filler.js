7eb41f6642682e3147b94964741cb0f53423f87b
Charles MASSON <c.masson@shizen.io>
2021-12-26 12:39:37 +0100

    Fix using above/below filling option with discontinuous lines
    Added a test in the boundary folder

--- src/plugins/plugin.filler.js
+++ src/plugins/plugin.filler.js
@@ -383,9 +383,30 @@ function resolveTarget(sources, index, propagate) {
 }
 
 function _clip(ctx, target, clipY) {
+  const {segments, points} = target;
+  let first = true;
+  let lineLoop = false;
+
   ctx.beginPath();
-  target.path(ctx);
-  ctx.lineTo(target.last().x, clipY);
+  for (const segment of segments) {
+    const {start, end} = segment;
+    const firstPoint = points[start];
+    const lastPoint = points[findSegmentEnd(start, end, points)];
+    if (first) {
+      ctx.moveTo(firstPoint.x, firstPoint.y);
+      first = false;
+    } else {
+      ctx.lineTo(firstPoint.x, clipY);
+      ctx.lineTo(firstPoint.x, firstPoint.y);
+    }
+    lineLoop = !!target.pathSegment(ctx, segment, {move: lineLoop});
+    if (lineLoop) {
+      ctx.closePath();
+    } else {
+      ctx.lineTo(lastPoint.x, clipY);
+    }
+  }
+
   ctx.lineTo(target.first().x, clipY);
   ctx.closePath();
   ctx.clip();
