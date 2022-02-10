d4bb967b973b67b1abe35088a1dbb3ba7e12baed
Talla2XLC <50024798+Talla2XLC@users.noreply.github.com>
2022-02-10 19:51:10 +0300

    Update types/index.esm.d.ts
    Co-authored-by: Jacco van den Berg <39033624+LeeLenaleee@users.noreply.github.com>

--- types/index.esm.d.ts
+++ types/index.esm.d.ts
@@ -3033,7 +3033,7 @@ export interface CartesianScaleOptions extends CoreScaleOptions {
      * The label alignment
      * @default 'center'
      */
-    align: 'start' | 'center' | 'end' | 'inner';
+    align: Align | 'inner';
     /**
      *   If true, automatically calculates how many labels can be shown and hides labels accordingly. Labels will be rotated up to maxRotation before skipping any. Turn autoSkip off to show all labels no matter what.
      * @default true
