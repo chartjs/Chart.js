6a6270ff991b41629b129aa62e200afed54c676e
Jacco van den Berg <jaccoberg2281@gmail.com>
2022-05-02 23:07:38 +0200

    export isPaternOrGradient helper with typings

--- src/helpers/helpers.color.js
+++ src/helpers/helpers.color.js
@@ -1,13 +1,13 @@
 import colorLib from '@kurkle/color';
 
-const isPatternOrGradient = (value) => {
+export function isPatternOrGradient(value) {
   if (value && typeof value === 'object') {
     const type = value.toString();
     return type === '[object CanvasPattern]' || type === '[object CanvasGradient]';
   }
 
   return false;
-};
+}
 
 export function color(value) {
   return isPatternOrGradient(value) ? value : colorLib(value);
