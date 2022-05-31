f0e13be426073b6aa78fa0c61d3c582099d55a84
Jacco van den Berg <jaccoberg2281@gmail.com>
2022-05-31 18:45:44 +0200

    add extra test cases

--- types/tests/chart_types.ts
+++ types/tests/chart_types.ts
@@ -8,7 +8,8 @@ const chart = new Chart('chart', {
       data: [1, 2, 3]
     },
     {
-      data: [1, 2, 3]
+      data: [1, 2, 3],
+      categoryPercentage: 10
     }],
   }
 });
@@ -19,10 +20,18 @@ const chart2 = new Chart('chart', {
     labels: ['1', '2', '3'],
     datasets: [{
       type: 'line',
+      data: [1, 2, 3],
+      // @ts-expect-error should not allow bar properties to be defined in a line dataset
+      categoryPercentage: 10
+    },
+    {
+      type: 'line',
+      pointBackgroundColor: 'red',
       data: [1, 2, 3]
     },
     {
-      data: [1, 2, 3]
+      data: [1, 2, 3],
+      categoryPercentage: 10
     }],
   }
 });
@@ -32,10 +41,28 @@ const chart3 = new Chart('chart', {
     labels: ['1', '2', '3'],
     datasets: [{
       type: 'bar',
-      data: [1, 2, 3]
+      data: [1, 2, 3],
+      categoryPercentage: 10
     },
     {
       type: 'bar',
+      data: [1, 2, 3],
+      // @ts-expect-error should not allow line properties to be defined in a bar dataset
+      pointBackgroundColor: 'red',
+    }],
+  }
+});
+
+// @ts-expect-error all datasets should have a type property or a default fallback type should be set
+const chart4 = new Chart('chart', {
+  data: {
+    labels: ['1', '2', '3'],
+    datasets: [{
+      type: 'bar',
+      data: [1, 2, 3],
+      categoryPercentage: 10
+    },
+    {
       data: [1, 2, 3]
     }],
   }
