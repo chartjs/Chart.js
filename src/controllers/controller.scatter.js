110c1afbaa8e495cdaa308ac1d95c6e8f3ebc4df
v.terekhov <v.terekhov@iportal.ru>
2022-06-21 14:09:52 +0400

    feat: remove line element from scatter controller default config

--- src/controllers/controller.scatter.js
+++ src/controllers/controller.scatter.js
@@ -1,7 +1,133 @@
-import LineController from './controller.line';
+import DatasetController from '../core/core.datasetController';
+import {isNullOrUndef} from '../helpers';
+import {_limitValue, isNumber} from '../helpers/helpers.math';
+import {_lookupByKey} from '../helpers/helpers.collection';
 
-export default class ScatterController extends LineController {
+export default class ScatterController extends DatasetController {
+  initialize() {
+    super.initialize();
 
+    const {dataset} = this._cachedMeta || null;
+    const {showLine} = this.options || false;
+
+    if (showLine && dataset === null) {
+      const config = {
+        datasetElementType: 'line',
+        dataElementType: 'point',
+        showLine: true,
+        fill: false,
+      };
+      this.chart.buildOrUpdateControllers(config);
+    }
+  }
+
+  update(mode) {
+    const meta = this._cachedMeta;
+    const {data: points = []} = meta;
+    // @ts-ignore
+    const animationsDisabled = this.chart._animationsDisabled;
+    let {start, count} = getStartAndCountOfVisiblePoints(meta, points, animationsDisabled);
+
+    this._drawStart = start;
+    this._drawCount = count;
+
+    if (scaleRangesChanged(meta)) {
+      start = 0;
+      count = points.length;
+    }
+
+    if (this.options.showLine) {
+
+      const {dataset: line, _dataset} = meta;
+
+      // Update Line
+      line._chart = this.chart;
+      line._datasetIndex = this.index;
+      line._decimated = !!_dataset._decimated;
+      line.points = points;
+
+      const options = this.resolveDatasetElementOptions(mode);
+      options.segment = this.options.segment;
+      this.updateElement(line, undefined, {
+        animated: !animationsDisabled,
+        options
+      }, mode);
+    } else {
+      this.options.borderWidth = 0;
+    }
+
+    // Update Points
+    this.updateElements(points, start, count, mode);
+  }
+
+  updateElements(points, start, count, mode) {
+    const reset = mode === 'reset';
+    const {iScale, vScale, _stacked, _dataset} = this._cachedMeta;
+    const firstOpts = this.resolveDataElementOptions(start, mode);
+    const sharedOptions = this.getSharedOptions(firstOpts);
+    const includeOptions = this.includeOptions(mode, sharedOptions);
+    const iAxis = iScale.axis;
+    const vAxis = vScale.axis;
+    const {spanGaps, segment} = this.options;
+    const maxGapLength = isNumber(spanGaps) ? spanGaps : Number.POSITIVE_INFINITY;
+    const directUpdate = this.chart._animationsDisabled || reset || mode === 'none';
+    let prevParsed = start > 0 && this.getParsed(start - 1);
+
+    for (let i = start; i < start + count; ++i) {
+      const point = points[i];
+      const parsed = this.getParsed(i);
+      const properties = directUpdate ? point : {};
+      const nullData = isNullOrUndef(parsed[vAxis]);
+      const iPixel = properties[iAxis] = iScale.getPixelForValue(parsed[iAxis], i);
+      const vPixel = properties[vAxis] = reset || nullData ? vScale.getBasePixel() : vScale.getPixelForValue(_stacked ? this.applyStack(vScale, parsed, _stacked) : parsed[vAxis], i);
+
+      properties.skip = isNaN(iPixel) || isNaN(vPixel) || nullData;
+      properties.stop = i > 0 && (Math.abs(parsed[iAxis] - prevParsed[iAxis])) > maxGapLength;
+      if (segment) {
+        properties.parsed = parsed;
+        properties.raw = _dataset.data[i];
+      }
+
+      if (includeOptions) {
+        properties.options = sharedOptions || this.resolveDataElementOptions(i, point.active ? 'active' : mode);
+      }
+
+      if (!directUpdate) {
+        this.updateElement(point, i, properties, mode);
+      }
+
+      prevParsed = parsed;
+    }
+
+    this.updateSharedOptions(sharedOptions, mode, firstOpts);
+  }
+
+  /**
+	 * @protected
+	 */
+  getMaxOverflow() {
+    const meta = this._cachedMeta;
+    const data = meta.data || [];
+
+    if (!this.options.showLine) {
+      let max = 0;
+      for (let i = data.length - 1; i >= 0; --i) {
+        max = Math.max(max, data[i].size(this.resolveDataElementOptions(i)) / 2);
+      }
+      return max > 0 && max;
+    }
+
+    const dataset = meta.dataset;
+    const border = dataset.options && dataset.options.borderWidth || 0;
+
+    if (!data.length) {
+      return border;
+    }
+
+    const firstPoint = data[0].size(this.resolveDataElementOptions(0));
+    const lastPoint = data[data.length - 1].size(this.resolveDataElementOptions(data.length - 1));
+    return Math.max(border, firstPoint, lastPoint) / 2;
+  }
 }
 
 ScatterController.id = 'scatter';
@@ -10,6 +136,8 @@ ScatterController.id = 'scatter';
  * @type {any}
  */
 ScatterController.defaults = {
+  datasetElementType: false,
+  dataElementType: 'point',
   showLine: false,
   fill: false
 };
@@ -45,3 +173,54 @@ ScatterController.overrides = {
     }
   }
 };
+
+function getStartAndCountOfVisiblePoints(meta, points, animationsDisabled) {
+  const pointCount = points.length;
+
+  let start = 0;
+  let count = pointCount;
+
+  if (meta._sorted) {
+    const {iScale, _parsed} = meta;
+    const axis = iScale.axis;
+    const {min, max, minDefined, maxDefined} = iScale.getUserBounds();
+
+    if (minDefined) {
+      start = _limitValue(Math.min(
+        _lookupByKey(_parsed, iScale.axis, min).lo,
+        animationsDisabled ? pointCount : _lookupByKey(points, axis, iScale.getPixelForValue(min)).lo),
+      0, pointCount - 1);
+    }
+    if (maxDefined) {
+      count = _limitValue(Math.max(
+        _lookupByKey(_parsed, iScale.axis, max).hi + 1,
+        animationsDisabled ? 0 : _lookupByKey(points, axis, iScale.getPixelForValue(max)).hi + 1),
+      start, pointCount) - start;
+    } else {
+      count = pointCount - start;
+    }
+  }
+
+  return {start, count};
+}
+
+function scaleRangesChanged(meta) {
+  const {xScale, yScale, _scaleRanges} = meta;
+  const newRanges = {
+    xmin: xScale.min,
+    xmax: xScale.max,
+    ymin: yScale.min,
+    ymax: yScale.max
+  };
+  if (!_scaleRanges) {
+    meta._scaleRanges = newRanges;
+    return true;
+  }
+  const changed = _scaleRanges.xmin !== xScale.min
+		|| _scaleRanges.xmax !== xScale.max
+		|| _scaleRanges.ymin !== yScale.min
+		|| _scaleRanges.ymax !== yScale.max;
+
+  Object.assign(_scaleRanges, newRanges);
+  return changed;
+}
--- src/core/core.controller.js
+++ src/core/core.controller.js
@@ -392,7 +392,7 @@ class Chart {
     });
   }
 
-  buildOrUpdateControllers() {
+  buildOrUpdateControllers(config) {
     const newControllers = [];
     const datasets = this.data.datasets;
     let i, ilen;
@@ -420,7 +420,7 @@ class Chart {
         meta.controller.linkScales();
       } else {
         const ControllerClass = registry.getController(type);
-        const {datasetElementType, dataElementType} = defaults.datasets[type];
+        const {datasetElementType, dataElementType} = config || defaults.datasets[type];
         Object.assign(ControllerClass.prototype, {
           dataElementType: registry.getElement(dataElementType),
           datasetElementType: datasetElementType && registry.getElement(datasetElementType)
