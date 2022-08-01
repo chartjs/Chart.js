9418f9727fa3f00b5fc2ed840641468a08117e6d
v.terekhov <thabarbados@gmail.com>
2022-08-01 13:32:13 +0400

    fix: update getStartAndCountOFVisiblePoints helper code

--- src/helpers/helpers.extras.js
+++ src/helpers/helpers.extras.js
@@ -117,8 +117,8 @@ export function _getStartAndCountOfVisiblePoints(meta, points, animationsDisable
     }
     if (maxDefined) {
       count = _limitValue(Math.max(
-        _lookupByKey(_parsed, iScale.axis, max).hi + 1,
-        animationsDisabled ? 0 : _lookupByKey(points, axis, iScale.getPixelForValue(max)).hi + 1),
+        _lookupByKey(_parsed, iScale.axis, max, true).hi + 1,
+        animationsDisabled ? 0 : _lookupByKey(points, axis, iScale.getPixelForValue(max), true).hi + 1),
       start, pointCount) - start;
     } else {
       count = pointCount - start;
