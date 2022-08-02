e7a5438a93f9932e55294f479dcdfa098649eb55
Danya Olekh <danyaolekhq@gmail.com>
2022-08-02 11:17:51 +0300

    test branch

--- src/scales/index.js
+++
@@ -1,6 +0,0 @@
-export {default as CategoryScale} from './scale.category';
-export {default as LinearScale} from './scale.linear';
-export {default as LogarithmicScale} from './scale.logarithmic';
-export {default as RadialLinearScale} from './scale.radialLinear';
-export {default as TimeScale} from './scale.time';
-export {default as TimeSeriesScale} from './scale.timeseries';
