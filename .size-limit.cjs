cfd8613648f7088ba2dea5b02e65599c2b237c25
dangreen <danon0404@gmail.com>
2022-08-04 17:44:32 +0400

    ci: test size-limit only for ESM bundle

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -7,46 +7,40 @@ function modifyWebpackConfig(config) {
 module.exports = [
   {
     path: 'dist/chart.js',
-    limit: '94.8 KB',
-    webpack: false,
-    running: false
-  },
-  {
-    path: 'dist/chart.esm.js',
     limit: '75 KB',
     webpack: false,
     running: false
   },
   {
-    path: 'dist/chart.esm.js',
+    path: 'dist/chart.js',
     limit: '34 KB',
     import: '{ Chart }',
     running: false,
     modifyWebpackConfig
   },
   {
-    path: 'dist/chart.esm.js',
+    path: 'dist/chart.js',
     limit: '19.5 KB',
     import: '{ BarController, BubbleController, DoughnutController, LineController, PolarAreaController, PieController, RadarController, ScatterController }',
     running: false,
     modifyWebpackConfig
   },
   {
-    path: 'dist/chart.esm.js',
+    path: 'dist/chart.js',
     limit: '14 KB',
     import: '{ ArcElement, LineElement, PointElement, BarElement }',
     running: false,
     modifyWebpackConfig
   },
   {
-    path: 'dist/chart.esm.js',
+    path: 'dist/chart.js',
     limit: '27 KB',
     import: '{ Decimation, Filler, Legend, SubTitle, Title, Tooltip }',
     running: false,
     modifyWebpackConfig
   },
   {
-    path: 'dist/chart.esm.js',
+    path: 'dist/chart.js',
     limit: '22 KB',
     import: '{ CategoryScale, LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale }',
     running: false,
