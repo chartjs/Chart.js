ba3394c638ecea849e321ac81624dfecceff5d42
t-mangoe <m.takashi.xpair@gmail.com>
2022-11-12 09:11:34 +0900

    check the data set type in DoughnutController

--- test/specs/mixed.tests.js
+++ test/specs/mixed.tests.js
@@ -1,3 +1,46 @@
 describe('Mixed charts', function() {
   describe('auto', jasmine.fixture.specs('mixed'));
+
+  it('shoud be constructed with doughnuts chart', function() {
+    const chart = window.acquireChart({
+      data: {
+        datasets: [{
+          type: 'line',
+          data: [10, 20, 30, 40],
+        }, {
+          type: 'doughnut',
+          data: [10, 20, 30, 50],
+        }
+        ],
+        labels: []
+      }
+    });
+
+    const meta0 = chart.getDatasetMeta(0);
+    expect(meta0.type).toEqual('line');
+    const meta1 = chart.getDatasetMeta(1);
+    expect(meta1.type).toEqual('doughnut');
+  });
+
+  it('shoud be constructed with pie chart', function() {
+    const chart = window.acquireChart({
+      data: {
+        datasets: [{
+          type: 'bar',
+          data: [10, 20, 30, 40],
+        }, {
+          type: 'pie',
+          data: [10, 20, 30, 50],
+        }
+        ],
+        labels: []
+      }
+    });
+
+    const meta0 = chart.getDatasetMeta(0);
+    expect(meta0.type).toEqual('bar');
+    const meta1 = chart.getDatasetMeta(1);
+    expect(meta1.type).toEqual('pie');
+  });
+
 });
