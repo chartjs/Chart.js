d0395bcd2f7bfde7cd0bb5bc7cd8b2d955575482
Dustin Ewan <dustineewan@gmail.com>
2023-09-28 11:36:38 -0500

    fix(#10896): correct typing for doughnut, pie, and polarArea charts

--- test/types/options.ts
+++ test/types/options.ts
@@ -1,33 +1,53 @@
-import { Chart } from '../../src/types.js';
+import {
+  Chart,
+  ChartOptions,
+  ChartType,
+  DoughnutControllerChartOptions,
+} from '../../src/types.js';
 
 const chart = new Chart('test', {
   type: 'bar',
   data: {
     labels: ['a'],
-    datasets: [{
-      data: [1],
-    }, {
-      type: 'line',
-      data: [{ x: 1, y: 1 }]
-    }]
+    datasets: [
+      {
+        data: [1],
+      },
+      {
+        type: 'line',
+        data: [{ x: 1, y: 1 }],
+      },
+    ],
   },
   options: {
     animation: {
-      duration: 500
+      duration: 500,
     },
     backgroundColor: 'red',
     datasets: {
       line: {
         animation: {
-          duration: 600
+          duration: 600,
         },
         backgroundColor: 'blue',
-      }
+      },
     },
     elements: {
       point: {
-        backgroundColor: 'red'
-      }
-    }
-  }
+        backgroundColor: 'red',
+      },
+    },
+  },
 });
+
+const doughnutOptions: DoughnutControllerChartOptions = {
+  circumference: 360,
+  cutout: '50%',
+  offset: 0,
+  radius: 100,
+  rotation: 0,
+  spacing: 0,
+  animation: false,
+};
+
+const chartOptions: ChartOptions<ChartType> = doughnutOptions;
