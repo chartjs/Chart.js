6daf4ba4e3993118e4a5f4b7783003eba2511864
EricWittrock <92955915+EricWittrock@users.noreply.github.com>
2024-04-28 23:36:56 -0500

    #11450 hide bar by dataindex (#11755)

--- src/controllers/controller.bar.js
+++ src/controllers/controller.bar.js
@@ -647,7 +647,7 @@ export default class BarController extends DatasetController {
     let i = 0;
 
     for (; i < ilen; ++i) {
-      if (this.getParsed(i)[vScale.axis] !== null) {
+      if (this.getParsed(i)[vScale.axis] !== null && !rects[i].hidden) {
         rects[i].draw(this._ctx);
       }
     }
