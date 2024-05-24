ff740789a5ef722e52b6328417ed148a93a2b926
Jacco van den Berg <jaccoberg2281@gmail.com>
2024-05-24 16:15:28 +0200

    Fix drawing angle lines on reversed radial scale (#11788)
    * Fix drawing angle lines on reversed radial scale
    
    * add test
    
    * Show ticks in test
    
    ---------
    
    Co-authored-by: Jacco van den Berg <jacco@jem-id.nl>

--- src/scales/scale.radialLinear.js
+++ src/scales/scale.radialLinear.js
@@ -606,7 +606,7 @@ export default class RadialLinearScale extends LinearScaleBase {
         ctx.setLineDash(optsAtIndex.borderDash);
         ctx.lineDashOffset = optsAtIndex.borderDashOffset;
 
-        offset = this.getDistanceFromCenterForValue(opts.ticks.reverse ? this.min : this.max);
+        offset = this.getDistanceFromCenterForValue(opts.reverse ? this.min : this.max);
         position = this.getPointPosition(i, offset);
         ctx.beginPath();
         ctx.moveTo(this.xCenter, this.yCenter);
