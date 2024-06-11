7d74fa5698befea3b9951130c5dc804b3ee5e79a
Abed <abed_al_zag@yahoo.com>
2024-06-11 21:08:29 +0300

    chore:testing

--- src/index.umd.ts
+++ src/index.umd.ts
@@ -4,30 +4,30 @@
 /**
  * @namespace Chart
  */
-import Chart from './core/core.controller.js';
+import Chart from "./core/core.controller.js";
 
-import * as helpers from './helpers/index.js';
-import _adapters from './core/core.adapters.js';
-import Animation from './core/core.animation.js';
-import animator from './core/core.animator.js';
-import Animations from './core/core.animations.js';
-import * as controllers from './controllers/index.js';
-import DatasetController from './core/core.datasetController.js';
-import Element from './core/core.element.js';
-import * as elements from './elements/index.js';
-import Interaction from './core/core.interaction.js';
-import layouts from './core/core.layouts.js';
-import * as platforms from './platform/index.js';
-import * as plugins from './plugins/index.js';
-import registry from './core/core.registry.js';
-import Scale from './core/core.scale.js';
-import * as scales from './scales/index.js';
-import Ticks from './core/core.ticks.js';
+import * as helpers from "./helpers/index.js";
+import _adapters from "./core/core.adapters.js";
+import Animation from "./core/core.animation.js";
+import animator from "./core/core.animator.js";
+import Animations from "./core/core.animations.js";
+import * as controllers from "./controllers/index.js";
+import DatasetController from "./core/core.datasetController.js";
+import Element from "./core/core.element.js";
+import * as elements from "./elements/index.js";
+import Interaction from "./core/core.interaction.js";
+import layouts from "./core/core.layouts.js";
+import * as platforms from "./platform/index.js";
+import * as plugins from "./plugins/index.js";
+import registry from "./core/core.registry.js";
+import Scale from "./core/core.scale.js";
+import * as scales from "./scales/index.js";
+import Ticks from "./core/core.ticks.js";
 
 // Register built-ins
 Chart.register(controllers, scales, elements, plugins);
 
-Chart.helpers = {...helpers};
+Chart.helpers = { ...helpers };
 Chart._adapters = _adapters;
 Chart.Animation = Animation;
 Chart.Animations = Animations;
@@ -46,9 +46,8 @@ Chart.Ticks = Ticks;
 Object.assign(Chart, controllers, scales, elements, plugins, platforms);
 Chart.Chart = Chart;
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   window.Chart = Chart;
 }
 
 export default Chart;
-
