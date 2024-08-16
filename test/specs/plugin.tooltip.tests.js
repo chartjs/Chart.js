f7ae0a56eb91656f6c5e2c00391f5b0fc9b94756
Jacco van den Berg <jacco@jem-id.nl>
2024-08-16 09:11:35 +0200

    Return false on no data

--- test/specs/plugin.tooltip.tests.js
+++ test/specs/plugin.tooltip.tests.js
@@ -1144,6 +1144,14 @@ describe('Plugin.Tooltip', function() {
       expect(tooltipModel.caretX).not.toBe(xPositionArrayAverage);
       expect(tooltipModel.caretX).toBe(xPositionSetAverage);
     });
+
+    it('Should not fail with all hiden data elements on the average positioner', function() {
+      const averagePositioner = tooltipPlugin.positioners.average;
+
+      // Simulate `hasValue` returns false
+      expect(averagePositioner([{x: 'invalidNumber', y: 'invalidNumber'}])).not.toThrow();
+      expect(averagePositioner([{x: 'invalidNumber', y: 'invalidNumber'}])).toBe(false);
+    });
   });
 
   it('Should avoid tooltip truncation in x axis if there is enough space to show tooltip without truncation', async function() {
